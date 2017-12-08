d78a840a4bb9b673468fa194bdea7a4711802fe9
Noah Tan <tempo0829@gmail.com>
2017-12-08 17:46:45 +0800

    test ssr meta

--- src/components/Proj1/Proj1.vue
+++ src/components/Proj1/Proj1.vue
@@ -4,8 +4,26 @@
   </div>
 </template>
 <script>
+  import titleMeta from '../../util/titleMeta'
   export default {
     name: '',
+    mixins: [ titleMeta ],
+    metaInfo () {
+      let description = 'proj 1 description'
+      let ogImage = 'marathon/images/og.jpg'
+      let metaUrl = 'proj1'
+      if (this.$route.query.q1) {
+        ogImage = 'marathon/images/og-chicago.jpg'
+        description = 'proj 1 q1'
+        metaUrl = `proj1?q1=${this.$route.query.q1}`
+      }
+      return {
+        title: 'proj 1',
+        description: description,
+        metaUrl: metaUrl,
+        metaImage: ogImage
+      }
+    },
     methods: {},
     mounted () {},
   }
