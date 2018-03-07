64fccd9f8bc14adced8327718d0780f73b962839
ChienHsin-Chan <hcchien@gmail.com>
2018-03-07 22:21:47 +0800

    modify the path of service-worker.js

--- src/entry-client.js
+++ src/entry-client.js
@@ -64,5 +64,5 @@ router.onReady(() => {
 
 // service worker
 if ('https:' === location.protocol && navigator.serviceWorker) {
-  navigator.serviceWorker.register('/dist/service-worker.js')
+  navigator.serviceWorker.register('service-worker.js')
 }
