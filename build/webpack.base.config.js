94d8f00f99af9a7360518cd464003c9ab997c28d
Noah Tan <tempo0829@gmail.com>
2018-03-08 22:57:28 +0800

    recover webpack js file config (#77)

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -35,8 +35,7 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel-loader',
         // exclude: /node_modules/
-        exclude: /node_modules\/(?!(dom7)\/).*/
-        
+        exclude: /node_modules\/(?!(dom7|swiper)\/).*/
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
