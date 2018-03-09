754a380dc09ce2a1cd7d72750f6d0e659296b476
Noah Tan <tempo0829@gmail.com>
2018-03-09 14:17:35 +0800

    fix current environment detect

--- src/util/comm.js
+++ src/util/comm.js
@@ -17,7 +17,7 @@ export function currDevice (osString) {
 
 export function currEnv () {
   if (process.env.VUE_ENV === 'client') {
-    if (location.host.indexOf(SITE_DOMAIN_PROD) === 0 || location.host.indexOf(`www.${SITE_DOMAIN_PROD}`) === 0) {
+    if (location.host.indexOf(SITE_DOMAIN_PROD) === 0 || location.host.indexOf(`projects.${SITE_DOMAIN_PROD}`) === 0) {
       return 'prod'
     } else {
       return 'dev'
