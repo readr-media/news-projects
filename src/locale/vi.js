b276aebe8243f42379a42254eb20fca4b22bba45
Noah Tan <tempo0829@gmail.com>
2018-05-03 18:03:57 +0800

    add foreign labour vietnamese version (#105)

---
+++ src/locale/vi.js
@@ -0,0 +1,28 @@
+export default {
+  'FOREIGN_LABOUR': {
+    'I18N': '中文',
+    'TITLE_1': 'Nghèo đến',
+    'TITLE_2': 'mức chỉ còn cái mạng sống',
+    'HEADER_CONTENT_1': 'Ở quê hương,họ nghèo đến mức chỉ còn cái mạng sống.',
+    'HEADER_CONTENT_2': 'Đến Hòn đảo này,cho dù họ kiếm được tiền ,nhưng chưa chắc còn mạng trở về nhà....',
+    'SECTION_2': 'Bố tôi nói là muốn đến Đài Loan để chăm sóc tôi,tôi nói rằng bố già rồi ,bố đừng qua đây nữa,bố à! giờ con cũng biết đi làm rồi, ngồi thuyền qua đây nguy hiểm lắm ,bố đừng qua, nhưng bố tôi nói rằng tôi ở bên này chỉ có một mình nên không an tâm,cho nên muốn đến chăm sóc và cùng làm việc với tôi.',
+    'SECTION_3_1_1': 'Rạng sáng Ngày 19 tháng 3 năm 2018hai người quốc tịch Việt Nam vượt biên ở khu vực Huyện Đài Đông,thôn Đàm Niên bị chết đuối,cùng hơn 20 mấy người vượt biên khác đến từ Việt Nam,và 2 người Đài Loan trong đường dây',
+    'SECTION_3_1_LINK': ' ',
+    'SECTION_3_1_2': ' tổ chức vượt biên cùng bị bắt.Một trong số người vượt biên chết đuối đó có PHAN ĐỒNG. ..,Vốn ở tỉnh Hà Tĩnh Việt Nam làm nghề đánh bắt cá mưu sinh,năm 2004 chuyển sang đi xuất khẩu lao động đến Đài Loan làm ngư công,chưa tới hai năm PHAN ĐỒNG....phát hiện tiền lương sau khi bị công ty môi giới trừ xong thì chẳng còn mấy đồng nên đã bỏ trốn và tứ xứ kiếm việc làm thêm và cuối cùng ở lại một vườn rau quả trên núi làm nông dân.',
+    'SECTION_3_2': 'Năm2015,PHAN VĂN THIỆN theo bước chân của người cha,bỏ ra 8000 đô la Mỹ phí môi giới đến Đài Loan,số mạng hai người này gần như giống nhau,PHAN VAN THIỆN chỉ làm ở tại công ty làm thùng giấy được 20 tháng,vì số tiền lương thực tế thu nhập nhận được quá thấp vì vậy cuộc sống bỏ trốn bắt đầu,và cuối cùng là cùng  giống PHAN ĐỒNG....lên núi trồng rau . Tháng 9 năm ngoái,PHAN ĐỒNG ...cùng với những người bạn cùng trồng rau đang nấu bữa tối thì cảnh sát ập đến,PHAN VĂN THIỆN cùng các đồng nghiệp nhanh chân chạy ra cửu sau thoát hiểm.',
+    'SECTION_4': 'Chiều tối ngày hôm đó nhận được tin bố mất,PHAN VĂN THIỆN vẫn đang bận trồng rau ở vườn, ngày thứ hai liền nghĩ    xuống núi tìm đến Cục Di Dân trước để đầu thú sau đó đến nhà tang lễ nhận xác cha,nhưng thật không may trên đường xuống núi thì bị cảnh sát bắt và phải mất cả nửa ngày giải thích thì cảnh sát mới tin rằng đang trên đường đi tự thú,cuối cùng cũng thả và để anh tự ngồi xe đến Đài Đông tự thú.',
+    'SECTION_5': 'Sau khi kết thúc hàng loạt các trình tự thủ tục tự thú xong,người của Cục Di Dân đưa PHAN VĂN THIỆN đến nhà tang lễ nhận xác cha,trên đường đi THIỆN nhìn như rất điềm tĩnh nhưng không ngờ khi cánh cửa tủ lạnh mở ra, THIỆN đã bị kích động không làm chủ được bản thân nữa.',
+    'SECTION_6': 'Hôm đó, PHAN VĂN THIỆN đã nằm ôm lấy di thể của bố mình và đã nói rất nhiều điều.',
+    'SECTION_7': 'Bố à! Tiền không quan trọng nữa rồi, để con đưa bố về nhà.',
+    'SECTION_8': '[PHAN ĐỒNG ..., tuy rằng bạn ở Đài Loan không may qua đời nhưng chúng tôi sẽ đưa linh hồn bạn về tới Việt Nam.]',
+    'SECTION_9': 'Ngày hoả thiêu của PHAN ĐỒNG ...,có rất nhiều anh chị em đồng hương đặc biệt xin nghỉ làm để đến gặp anh lần cuối .nhưng vì có rất nhiều cũng đang trong tình trạng bỏ trốn như anh nên không dám trực tiếp đến tiễn anh chặng đường cuối.',
+    'SECTION_10': 'Khi quan tài của PHAN ĐỒNG ... chuẩn bị đưa vào giàn hỏa thiêu trước đó thì PHAN VĂN THIỆN lại lần nữa không trấn áp được lỗi đau đớn của mình ,muốn chặn lại quan tài của người cha.',
+    'SECTION_11': 'Trong lúc hỏa thiêu,có tu nữ Việt Nam đến từ Bệnh Viện Thánh Mẫu tại Đài Đông  cùng các nhân viên của Bệnh viện đã không ngừng an ủi tâm trạng của THIỆN.',
+    'SECTION_12': 'Sau khi rời nhà tang lễ, PHAN VĂN THIỆN lưng cõng hài cốt của cha trên cả quãng đường và không hề bỏ xuống,một người thân lấy chồng qua Đài Loan đã mười mấy năm chủ động lấy xe của mình chở Thiện tới sân bay Đào viên đáp chuyến bay của ngày hôm sau nhưng cho dù đoạn đường tới hơn 10 tiếng nhưng Thiện chưa từng có khoảng khắc nào để cha mình xuống.',
+    'SECTION_13': 'Theo phong tục của Việt Nam, hài cốt của người đã mất trên đường về nhà phải không ngừng thả tiền coi như “tiền qua cầu đường”.Đêm đó ,trên tuyến đường số 11 đến tuyến quốc lộ số 1 không ngừng vang lên âm thanh của những đồng xu rơi xuống thanh giòn của cha con họ PHAN lần cuối cùng ở Đài Loan.',
+    'SECTION_14': 'Đêm đó, Thiện kiên quyết không chịu ở nhà bất kì ai qua đêm  mà tìm đến một góc yên tĩnh chờ đợi đến ngày mai.',
+    'SECTION_15_1': 'Trên cổ của Thiện có nổi trội một hình xăm hình mỏ neo thuyền.',
+    'SECTION_15_2': 'Thiện nói,trước khi đến Đài Loan cũng đã từng đến nhiều nước Đông Nam Á làm việc,hầu như nhiều người nhìn thấy hình xăm này đều liên tưởng đến nghề biển đánh bắt cá nhưng Thiện nói: [Tôi xăm hình này thực ra là không muốn đi biển đánh bắt cá nữa, vì hi vọng sẽ có ngày không phải trôi nổi nữa,muốn có được một bến đỗ, tôi muốn an định lại và có một mái nhà để tôi có thể trở về.]',
+    'SECTION_16': 'Thực ra tôi biết bố tôi làm việc rất cực khổ,và cũng là để muốn tiết kiệm tiền cho tôi xây nhà và cưới vợ, vì chúng tôi ở Việt Nam nếu không nhà thì cũng như không thể kết hôn và có mái ấm gia đình. Giờ bố tôi như vậy rồi,không có tiền tôi thấy cũng không sao rồi, tôi không muốn đi tứ xứ làm việc nữa rồi, tôi phải đưa bố tôi về nhà,sau này tôi ở Việt Nam đi biển đánh bắt cá cũng có thể sống rồi.'
+  }
+}
