dcb2ca9b4166992e2544368db6e9c06f9aac514d
Noah Tan <tempo0829@gmail.com>
2018-05-03 18:04:09 +0800

    add foreign labour vietnamese version

--- src/components/ForeignLabour/ForeignLabour.vue
+++ src/components/ForeignLabour/ForeignLabour.vue
@@ -1,13 +1,14 @@
 <template>
-  <main class="foreign-labour" :class="{ 'in-action': inAction }">
+  <main class="foreign-labour" :class="{ 'in-action': inAction, 'vietnamese': isVietnamese }">
     <app-logo class="no-sprite" href="https://www.readr.tw/" top="20px" left="20px" bgImage="/proj-assets/logo_readr.png"></app-logo>
+    <a :href="isVietnamese ? '/project/foreign-labour/': '/project/foreign-labour/vn/'" class="foreign-labour__i18n" v-text="$t('FOREIGN_LABOUR.I18N')"></a>
     <app-share :shareUrl="shareLink" top="15px" right="15px" bgColor="#000" direction="down"></app-share>
     <header class="foreign-labour__header">
       <div class="foreign-labour__header-content">
         <!-- <img :src="`/proj-assets/foreign-labour/images/landing-title${imgSelector}.png`" alt=""> -->
-        <h1>窮得只剩一條命</h1>
-        <p>在故鄉，他們窮得只剩一條命。</p>
-        <p>在這座島，他們即便能掙到錢，也不一定有命回家⋯⋯</p>
+        <h1>{{ $t('FOREIGN_LABOUR.TITLE_1') }}<br>{{ $t('FOREIGN_LABOUR.TITLE_2') }}</h1>
+        <p v-text="$t('FOREIGN_LABOUR.HEADER_CONTENT_1')"></p>
+        <p v-text="$t('FOREIGN_LABOUR.HEADER_CONTENT_2')"></p>
       </div>
     </header>
     <section class="foreign-labour__sec sec-full sec-active">
@@ -20,13 +21,13 @@
     <section class="foreign-labour__sec sec-full sec-text-border">
       <div class="foreign-labour__text-border">
         <div class="foreign-labour__text">
-          <p>我爸爸說他要來台灣照顧我，我說他老了不要再來，爸爸我已經會工作，坐船很危險不要來，可是他說我在這邊一個人，不放心，所以要來照顧我，跟我一起工作。</p>
+          <p v-text="$t('FOREIGN_LABOUR.SECTION_2')"></p>
         </div>
       </div>
     </section>
     <section class="foreign-labour__sec sec-text">
-      <p>2018 年 3 月 19 日凌晨，兩名越南籍偷渡客在台東縣潭年村塔瓦溪出海口溺斃，同行二十幾名同樣來自越南的偷渡客，還有兩名台灣籍人蛇集團成員也一併被逮<a href="https://www.mirrormedia.mg/story/foreign-labour-escape/" target="_blank">（延伸閱讀）</a>。溺斃的其中一名越南人名叫潘同甘，原本在越南河靜省的老家捕魚維生，2004 年時輾轉來到台灣當漁民。沒兩年，潘同甘發現薪資扣完仲介費後所剩無幾，於是逃跑四處打零工，最後落腳在山區菜園當農民。</p>
-      <p>2015 年，潘文善跟上老爸腳步，花了 8,000 美金仲介費來到台灣。兩人命運相似，潘文善只在第一家紙箱工廠待了 20 個月，就因為實領所得太低而展開逃逸生活，最後索性和潘同甘一起上山種菜。去年 9 月，潘同甘在宿舍為其他同鄉的農工們煮晚餐，遭警方突襲逮補，潘文善和同鄉們連忙從後門逃跑，躲過一劫。潘文善說，爸爸回去沒多久，就一直擔心他在台灣過得太苦，逃逸生活又非常壓抑，主動提議要偷渡來台灣照顧他。未想，潘同甘再次踏上台灣土地的那一刻就沒了呼吸，再也無法照顧兒子了。</p>
+      <p>{{ $t('FOREIGN_LABOUR.SECTION_3_1_1') }}<a href="https://www.mirrormedia.mg/story/foreign-labour-escape/" target="_blank" v-text="$t('FOREIGN_LABOUR.SECTION_3_1_LINK')"></a>{{ $t('FOREIGN_LABOUR.SECTION_3_1_2') }}</p>
+      <p v-text="$t('FOREIGN_LABOUR.SECTION_3_2')"></p>
     </section>
     <section class="foreign-labour__sec sec-full">
       <figure class="foreign-labour__media" @click="$_foreignLabour_closeDescription">
@@ -36,7 +37,7 @@
         <!-- <h2>自首</h2> -->
         <button @click.stop="$_foreignLabour_toggleDescription($event, 1)"></button>
         <div>
-          <p>收到爸爸死訊的那天傍晚，潘文善還在農地裡忙著種菜。第二天，潘文善忙不迭趕下山，想先去移民署投案，再去殯儀館認屍。好巧不巧，潘文善在下山的路上就被警察逮補，解釋了老半天才讓警察相信他正要去投案，最後放他自行搭車前往台東自首。</p>
+          <p v-text="$t('FOREIGN_LABOUR.SECTION_4')"></p>
         </div>
       </div>
     </section>
@@ -48,7 +49,7 @@
         <!-- <h2>再見</h2> -->
         <button @click.stop="$_foreignLabour_toggleDescription($event, 2)"></button>
         <div>
-          <p>結束一連串投案自首的筆錄程序後，移民署官員帶著潘文善前往殯儀館認屍。一路上，潘文善看似沉穩淡定，沒想到冰櫃才剛打開，他就激動得無法自己。</p>
+          <p v-text="$t('FOREIGN_LABOUR.SECTION_5')"></p>
         </div>
       </div>
     </section>
@@ -60,14 +61,14 @@
         <!-- <h2>傾訴</h2> -->
         <button @click.stop="$_foreignLabour_toggleDescription($event, 3)"></button>
         <div>
-          <p>那天，潘文善伏臥在父親的遺體上，說了好多好多的話。</p>
+          <p v-text="$t('FOREIGN_LABOUR.SECTION_6')"></p>
         </div>
       </div>
     </section>
     <section class="foreign-labour__sec sec-full sec-text-border">
       <div class="foreign-labour__text-border">
         <div class="foreign-labour__text text-fix">
-          <p>爸爸，錢不重要了，我要帶你回家。</p>
+          <p v-text="$t('FOREIGN_LABOUR.SECTION_7')"></p>
         </div>
       </div>
     </section>
@@ -79,7 +80,7 @@
         <!-- <h2>蓋棺</h2> -->
         <button @click.stop="$_foreignLabour_toggleDescription($event, 4)"></button>
         <div>
-          <p>「潘同甘，雖然你在台灣不幸過世，但我們會帶著你的靈魂返回越南。」</p>
+          <p v-text="$t('FOREIGN_LABOUR.SECTION_8')"></p>
         </div>
       </div>
     </section>
@@ -91,7 +92,7 @@
         <!-- <h2>送行</h2> -->
         <button @click.stop="$_foreignLabour_toggleDescription($event, 5)"></button>
         <div>
-          <p>潘同甘要火化的那天，他的許多越南同鄉都特地請假來見他最後一面。只是，勢必有更多和他一樣處於逃跑狀態的人，無法親自到場送他最後一程。</p>
+          <p v-text="$t('FOREIGN_LABOUR.SECTION_9')"></p>
         </div>
       </div>
     </section>
@@ -103,7 +104,7 @@
         <!-- <h2>不捨</h2> -->
         <button @click.stop="$_foreignLabour_toggleDescription($event, 6)"></button>
         <div>
-          <p>潘同甘的棺木準備送進焚化爐的前一刻，潘文善再也壓抑不住自己的悲傷，想要攔住父親的棺木。</p>
+          <p v-text="$t('FOREIGN_LABOUR.SECTION_10')"></p>
         </div>
       </div>
     </section>
@@ -115,7 +116,7 @@
         <!-- <h2>慰</h2> -->
         <button @click.stop="$_foreignLabour_toggleDescription($event, 7)"></button>
         <div>
-          <p>火化的過程中，來自台東聖母醫院的越南籍修女，以及同樣在醫院工作的服務人員，不停安撫潘文善的情緒。</p>
+          <p v-text="$t('FOREIGN_LABOUR.SECTION_11')"></p>
         </div>
       </div>
     </section>
@@ -127,7 +128,7 @@
         <!-- <h2>背負</h2> -->
         <button @click.stop="$_foreignLabour_toggleDescription($event, 8)"></button>
         <div>
-          <p>離開殯儀館後，潘文善一路背著爸爸的骨灰不願放下。一名嫁到台灣十幾年的親戚主動提供自己的車，打算載著潘文善去桃園機場搭乘隔日的班機。即便在車上將近十小時的路程中，潘文善一刻也沒有放下過父親。</p>
+          <p v-text="$t('FOREIGN_LABOUR.SECTION_12')"></p>
         </div>
       </div>
     </section>
@@ -141,7 +142,7 @@
       <div class="foreign-labour__descr">
         <!-- <h2>上路</h2> -->
         <div>
-          <p>按照越南習俗，亡者的骨灰在回到家的路上，一路上要不停丟擲錢幣作為「過路費」。那天夜裡，從台 11 線一路到國道 1 號的路面上，陸續響起了銅板落地的清脆聲響——那是潘家父子在台灣最後的足聲。</p>
+          <p v-text="$t('FOREIGN_LABOUR.SECTION_13')"></p>
         </div>
       </div>
     </section>
@@ -156,7 +157,7 @@
         <!-- <h2>歇</h2> -->
         <button @click.stop="$_foreignLabour_toggleDescription($event, 9)"></button>
         <div>
-          <p>夜裏，潘文善堅持不肯到任何人家裡留宿，一行人索性直接找個安靜的角落，彼此無言地等待天明。</p>
+          <p v-text="$t('FOREIGN_LABOUR.SECTION_14')"></p>
         </div>
       </div>
     </section>
@@ -168,15 +169,15 @@
         <!-- <h2>靠岸</h2> -->
         <button @click.stop="$_foreignLabour_toggleDescription($event, 10)"></button>
         <div>
-          <p>潘文善的脖子上有個十分惹眼的船錨刺青。</p>
-          <p>他說，在到台灣前，他也曾在東南亞其他國家的工廠裡打工，但多數人看到這刺青時，聯想到的，都是他在老家的漁民身份。潘文善卻說：「我刺這個其實不是想要再出海打漁，而是希望不要再漂泊，有一個可以靠岸的港口⋯⋯我想安定下來，有一個家可以給我回去。」</p>
+          <p v-text="$t('FOREIGN_LABOUR.SECTION_15_1')"></p>
+          <p v-text="$t('FOREIGN_LABOUR.SECTION_15_2')"></p>
         </div>
       </div>
     </section>
     <section class="foreign-labour__sec sec-full sec-text-border">
       <div class="foreign-labour__text-border">
         <div class="foreign-labour__text">
-          <p>其實我知道爸爸辛苦工作，也是想讓我可以存錢蓋房子娶老婆，因為我們在越南沒有房子，就沒辦法結婚有自己的家。現在爸爸這樣，沒有錢我覺得也沒關係了，我不想要再四處工作了，我要帶爸爸回家，以後我在越南海邊抓魚也可以生活了。</p>
+          <p v-text="$t('FOREIGN_LABOUR.SECTION_16')"></p>
         </div>
       </div>
       <p class="foreign-labour__credit">文字、攝影：鐘聖雄<span></span><br>網頁：HY Tan<span></span>設計：許玲瑋</p>
@@ -212,11 +213,18 @@
       'app-share': Share,
     },
     metaInfo () {
+      let metaImage = `${PROJECT_NAME}/images/ogimage2.jpg`
+      if (this.$route.params.params === 'vn') {
+        metaImage = `${PROJECT_NAME}/images/ogimage_vi.jpg`
+        this.$i18n.locale = 'vi'
+      } else {
+        this.$i18n.locale = 'zh-TW'
+      }
       return {
-        title: '窮得只剩一條命',
-        description: '在故鄉，他們窮得只剩一條命。在這座島，他們即便能掙到錢，也不一定有命回家⋯⋯',
+        title: `${this.$t('FOREIGN_LABOUR.TITLE_1')}${this.$t('FOREIGN_LABOUR.TITLE_2')}`,
+        description: `${this.$t('FOREIGN_LABOUR.HEADER_CONTENT_1')}${this.$t('FOREIGN_LABOUR.HEADER_CONTENT_2')}`,
         metaUrl: PROJECT_NAME,
-        metaImage: `${PROJECT_NAME}/images/ogimage2.jpg`
+        metaImage: metaImage
       }
     },
     data () {
@@ -235,8 +243,18 @@
       imgSelector () {
         return this.viewport[0] >= 900 ? '' : '-m'
       },
+      isVietnamese () {
+        return this.$route.params.params === 'vn'
+      },
       videoMuted () {
         return get(this.$store, [ 'state', 'useragent', 'isMobile' ], true)
+      }
+    },
+    beforeMount () {
+      if (this.$route.params.params === 'vn') {
+        this.$i18n.locale = 'vi'
+      } else {
+        this.$i18n.locale = 'zh-TW'
       }
     },
     mounted () {
@@ -257,7 +275,6 @@
       window.addEventListener('mouseover', () => this.inAction = true)
 
       window.ga('send', 'pageview')
-
       // setInterval(() => {
       //   if (this.inAction) {
       //     this.inAction = false
@@ -369,6 +386,25 @@
             opacity 1
     figure
       margin 0
+    &__i18n
+      display flex
+      justify-content center
+      align-items center
+      position fixed
+      top 15px
+      right 70px
+      width 44px
+      height 44px
+      z-index 999
+      color #fff
+      text-decoration none
+      background-color #000
+      border-radius 50%
+      cursor pointer
+      transition background-color .5s
+      &:hover
+        background-color #bb93bc
+
     &__header
       position absolute
       top 0
@@ -391,6 +427,8 @@
           margin 0 auto
           font-size 16vw
           font-family "source-han-serif-tc", serif
+          br
+            display none
         p
           margin 0
           color #fff
@@ -617,6 +655,18 @@
         > div
           display block
 
+  .foreign-labour.vietnamese
+    .foreign-labour__header-content
+      h1
+        position relative
+        left -5%
+        max-width 110%
+        width 110%
+        font-size 2rem
+        line-height 1.3
+        br
+          display inline
+
   @media (min-width: 768px)
     .foreign-labour
       &__header-content
@@ -706,4 +756,12 @@
         font-size 1rem
         br
           display none
+    .foreign-labour.vietnamese
+      .foreign-labour__header-content
+        width 58%
+        h1
+          left 0
+          max-width none
+          width auto
+          font-size 4.125rem
 </style>
