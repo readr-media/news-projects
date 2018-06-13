1a715042fa285658fbbb11864a4b3d38e2fc7fac
Noah Tan <tempo0829@gmail.com>
2018-06-13 14:42:50 +0800

    improve meta mixin

--- src/util/titleMeta.js
+++ src/util/titleMeta.js
@@ -1,5 +1,8 @@
-import { MM_FB_APP_ID, MM_FB_PAGE_ID, MM_SITE_NAME, PROJECTS_GAID_BELONGS_MM, READR_FB_APP_ID, READR_FB_PAGE_ID, READR_SITE_ASSETS_URL, READR_SITE_NAME, READR_SITE_URL, } from '../constants'
-import _ from 'lodash'
+import { MM_FAVICON_URL, MM_FB_APP_ID, MM_FB_PAGE_ID, MM_FB_PAGE_URL, MM_SITE_ASSETS_URL, MM_SITE_NAME, MM_SITE_URL } from '../constants'
+import { PROJECTS_GAID_BELONGS_MM } from '../constants'
+import { READR_FAVICON_URL, READR_FB_APP_ID, READR_FB_PAGE_ID, READR_FB_PAGE_URL, READR_SITE_ASSETS_URL, READR_SITE_NAME, READR_SITE_URL } from '../constants'
+
+import { get } from 'lodash'
 
 function getMetaInfo (vm) {
   const { metaInfo } = vm.$options
@@ -14,37 +17,29 @@ const serverMetaInfoMixin = {
   created () {
     const metaInfo = getMetaInfo(this)
     if (metaInfo) {
-      const title = metaInfo.title
-      const description = metaInfo.description
-      const locale = metaInfo.locale || 'zh_TW'
-      const metaUrl = metaInfo.metaUrl
-      const metaImage = metaInfo.metaImage
-      const project = _.get(this.$route, [ 'params', 'project' ])
+      const project = get(this.$route, 'params.project')
+
       const siteName = PROJECTS_GAID_BELONGS_MM.includes(project) ? MM_SITE_NAME : READR_SITE_NAME
-      const favicon = metaInfo.favicon
+      const title = metaInfo.title ? `${metaInfo.title} - ${siteName}` : `${siteName}`
+      const description = metaInfo.description || ` `
+      const locale = metaInfo.locale || 'zh_TW'
+      const metaUrl = metaInfo.metaUrl || ` `
+      const metaImage = metaInfo.metaImage || ` `
+      const favicon = PROJECTS_GAID_BELONGS_MM.includes(project) ? MM_FAVICON_URL : READR_FAVICON_URL
       const fbAppId = PROJECTS_GAID_BELONGS_MM.includes(project) ? MM_FB_APP_ID : READR_FB_APP_ID
       const fbPageId = PROJECTS_GAID_BELONGS_MM.includes(project) ? MM_FB_PAGE_ID : READR_FB_PAGE_ID
-      
-      this.$ssrContext.siteName = siteName
-      
-      if (title) {
-        this.$ssrContext.title = `${title} - ${siteName}`
-      }
-      if (description) {
-        this.$ssrContext.description = description
-      }
-      if (metaUrl) {
-        this.$ssrContext.metaUrl = READR_SITE_URL + metaUrl
-      }
-      if (metaImage) {
-        this.$ssrContext.metaImage = READR_SITE_ASSETS_URL + metaImage
-      }
-      if (favicon) {
-        this.$ssrContext.favicon = favicon
-      }
+      const fbPageUrl = PROJECTS_GAID_BELONGS_MM.includes(project) ? MM_FB_PAGE_URL : READR_FB_PAGE_URL
+
+      this.$ssrContext.title = title
+      this.$ssrContext.description = description
       this.$ssrContext.locale = locale
+      this.$ssrContext.metaUrl = PROJECTS_GAID_BELONGS_MM.includes(project) ? MM_SITE_URL + metaUrl : READR_SITE_URL + metaUrl
+      this.$ssrContext.metaImage = PROJECTS_GAID_BELONGS_MM.includes(project) ? MM_SITE_ASSETS_URL + metaImage : READR_SITE_ASSETS_URL + metaImage
+      this.$ssrContext.siteName = siteName
+      this.$ssrContext.favicon = favicon
       this.$ssrContext.fbAppId = fbAppId
       this.$ssrContext.fbPageId = fbPageId
+      this.$ssrContext.fbPageUrl = fbPageUrl
     }
   }
 }
@@ -53,40 +48,33 @@ const clientMetaInfoMixin = {
   mounted () {
     const metaInfo = getMetaInfo(this)
     if (metaInfo) {
-      const title = metaInfo.title
-      const description = metaInfo.description
-      const locale = metaInfo.locale || 'zh_TW'
-      const metaUrl = metaInfo.metaUrl
-      const metaImage = metaInfo.metaImage
-      const project = _.get(this.$route, [ 'params', 'project' ])
+      const project = get(this.$route, 'params.project')
+
       const siteName = PROJECTS_GAID_BELONGS_MM.includes(project) ? MM_SITE_NAME : READR_SITE_NAME
-      const favicon = metaInfo.favicon
+      const title = metaInfo.title ? `${metaInfo.title} - ${siteName}` : `${siteName}`
+      const description = metaInfo.description || ` `
+      const locale = metaInfo.locale || 'zh_TW'
+      const metaUrl = metaInfo.metaUrl || ` `
+      const metaImage = metaInfo.metaImage || ` `
+      const favicon = PROJECTS_GAID_BELONGS_MM.includes(project) ? MM_FAVICON_URL : READR_FAVICON_URL
       const fbAppId = PROJECTS_GAID_BELONGS_MM.includes(project) ? MM_FB_APP_ID : READR_FB_APP_ID
       const fbPageId = PROJECTS_GAID_BELONGS_MM.includes(project) ? MM_FB_PAGE_ID : READR_FB_PAGE_ID
+      const fbPageUrl = PROJECTS_GAID_BELONGS_MM.includes(project) ? MM_FB_PAGE_URL : READR_FB_PAGE_URL
 
-      document.head.querySelector(`meta[property='og:site_name']`).content = siteName
-
-      if (title) {
-        document.title = `${title} - ${siteName}`
-        document.head.querySelector(`meta[property='og:title']`).content = `${title} - ${siteName}`
-      }
-      if (description) {
-        document.head.querySelector(`meta[name=description]`).content = description
-        document.head.querySelector(`meta[property='og:description']`).content = description
-      }
-      if (metaUrl) {
-        document.head.querySelector(`meta[property='og:url']`).content = READR_SITE_URL + metaUrl
-      }
-      if (metaImage) {
-        document.head.querySelector(`meta[property='og:image']`).content = READR_SITE_ASSETS_URL + metaImage
-      }
-      if (favicon) {
-        document.head.querySelector(`link[rel='apple-touch-icon']`).href = favicon
-        document.head.querySelector(`link[rel='shortcut icon']`).href = favicon
-      }
+      document.title = title
+      document.head.querySelector(`meta[property='og:title']`).content = title
+      document.head.querySelector(`meta[name=description]`).content = description
+      document.head.querySelector(`meta[property='og:description']`).content = description
       document.head.querySelector(`meta[property='og:locale']`).content = locale
+      document.head.querySelector(`meta[property='og:url']`).content = PROJECTS_GAID_BELONGS_MM.includes(project) ? MM_SITE_URL + metaUrl : READR_SITE_URL + metaUrl
+      document.head.querySelector(`meta[property='og:image']`).content = PROJECTS_GAID_BELONGS_MM.includes(project) ? MM_SITE_ASSETS_URL + metaImage : READR_SITE_ASSETS_URL + metaImage
+      document.head.querySelector(`meta[property='og:site_name']`).content = siteName
+      document.head.querySelector(`link[rel='apple-touch-icon']`).href = favicon
+      document.head.querySelector(`link[rel='shortcut icon']`).href = favicon
       document.head.querySelector(`meta[property='fb:app_id']`).content = fbAppId
       document.head.querySelector(`meta[property='fb:pages']`).content = fbPageId
+      document.head.querySelector(`meta[property='article:author']`).content = fbPageUrl
+      document.head.querySelector(`meta[property='article:publisher']`).content = fbPageUrl
     }
   }
 }
