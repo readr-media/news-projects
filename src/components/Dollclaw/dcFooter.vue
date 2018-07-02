609278c08cad5fece2fd7b602fdf4b58f1831fae
vinthen <nehtniv@gmail.com>
2018-07-02 10:54:03 +0800

    dollclaw change footer bg color

--- src/components/Dollclaw/dcFooter.vue
+++ src/components/Dollclaw/dcFooter.vue
@@ -16,20 +16,32 @@
     </div>
 
     <related-reports theme="dark"></related-reports>
+    <div class="centerwpr">
+        <div class="fb-comments" :data-href="commentsUrl" data-colorscheme="dark" data-numposts="5" data-order-by="reverse_time" data-width="100%"></div>
+    </div>
+
 </div>
     
 </template>
 
 <script>
 
+import { READR_DOMAIN, READR_SITE_URL } from '../../constants';
 import RelatedReports from '../RelatedReports.vue';
 
+const PROJECT_NAME = 'dollclaw';
 
 export default {
 
     components: {
       'related-reports': RelatedReports
     },
+
+    data: function(){
+        return {
+            commentsUrl: `https://dev.${READR_DOMAIN}/project/${PROJECT_NAME}/`,
+        }
+    }
     
 }
 </script>
@@ -39,14 +51,16 @@ export default {
 
 .footerwpr {position:relative; padding-bottom:50px;    
 text-align:left;    
-background-color:#fbe0db;
+/* background-color:#fbe0db; */
+background-color:#544356;
 }
 
 /* ---------- Credit ---------- */
 .credit {padding:30px 0;
 text-align:center;
 background-color:#60465a;
-border-bottom:10px solid #f9d3cc;
+/* border-bottom:10px solid #f9d3cc; */
+border-bottom:10px solid #49394b;
 }
 .credit .centerwpr:before {display:none;}
 .credit li {font-size:14px; color:#fff; letter-spacing:1px;}
@@ -63,9 +77,7 @@ margin-top:15px;
 
 @media screen and (max-width: 1000px) {
 
-    .footerwpr {background-color:#544356;}
 
-    .credit {border-bottom:10px solid #49394b;}
 
 }
 
