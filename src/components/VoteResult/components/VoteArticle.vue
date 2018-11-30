8b0931f3ecf8a2e38728140f202c04f6875f70eb
Noah Tan <tempo0829@gmail.com>
2018-11-30 18:45:27 +0800

    adjust vote result style and add comment (#368)

--- src/components/VoteResult/components/VoteArticle.vue
+++ src/components/VoteResult/components/VoteArticle.vue
@@ -17,20 +17,16 @@
     <p>依照中選會資料，我們將各村里公投案 14、15 案的同意票加上 10 、11、12 案反對票的人口加總平均，除以該村里總投票人口，可以得出該村裡的支持婚姻平權、支持實施同志教育的比率。</p>
     <p>支持婚姻平權程度最高的前 10 個村里，除了彰化縣竹塘鄉小西村之外，其他分佈於新竹市、高雄市、雙北都會區。對婚姻平權公投支持度最低的 10 個村里分佈於嘉義縣、澎湖縣、花蓮縣、金門縣、雲林縣、南投縣。</p>
     <p>全台灣支持婚姻平權比率最低的是南投縣仁愛鄉榮興村（ 3.1 %），榮興村 20 歲以上居民的平均年齡 59.5歲 ，家戶年收入中位數 48.9 萬元。 根據南投縣仁愛鄉公所官網，榮興村大部分村境位於太魯閣國家公園內，是人口稀少的散村聚落，只有 64 人，居民組成以榮民為主，30 歲以下的居民只有 3 人。榮興村有大學（含）以上學歷的人數 5 人，佔全部村民人口的 7.8 %。男女比則為 1.56。</p>
-    <blockquote>
-      <img src="/proj-assets/vote2018-result/quotation.png" alt="">
-      <p>支持婚姻平權比例最高的新竹市關新里居民，<br>大學以上學歷的里民佔了 78.7%</p>
-      <img src="/proj-assets/vote2018-result/quotation.png" alt="">
-    </blockquote>
+    <div class="quotation">
+      支持婚姻平權比例最高的新竹市關新里居民，<br>大學以上學歷的里民佔了 78.7%
+    </div>
     <p>支持婚姻平權程度最高的里則是新竹市東區關新里（ 27.92 %），20 歲以上居民的平均年齡 38.62 歲 ，收入中位數 244.3萬元。離新竹科學園區約十分鐘車程的關新里，居住者大多在竹科工作，關新里幾年來高居全台灣收入中位數最高排行榜，因此多次獲得媒體關注。</p>
     <p>相比榮興村，關新里里民眾多，有3639人，具有大學（含）以上學歷的人口佔全里人口數的 78.7 %。男女比 0.98 ，女性里民略多於男性。關新里的收入中位數高出榮興村近 5 倍，居民平均年齡較榮興村低 20.88 歲，有大學以上學歷的里民比例，關新里比榮興村高出 10 倍。</p>
     <h3>高雄多區力挺東奧正名</h3>
     <p>我們將各村里投下贊成東奧正名的有效票除以有投票權人數，算出贊成東奧正名的比率。數據顯示，對東奧正名支持度高的前 20 名村里中，高雄市就佔超過半數。20 個村里中只有 2 個里位於北台灣。</p>
-    <blockquote>
-      <img src="/proj-assets/vote2018-result/quotation.png" alt="">
-      <p>前 20 名支持東奧正名的里，有 13 個在高雄市</p>
-      <img src="/proj-assets/vote2018-result/quotation.png" alt="">
-    </blockquote>
+    <div class="quotation">
+      前 20 名支持東奧正名的里，有 13 個在高雄市
+    </div>
     <p>簡稱東奧正名的公投第 13 案由前體育國手紀政領銜提出，公投主文是：「你是否同意，以『台灣』（Taiwan）為全名申請參加所有國際運動賽事及 2020 年東京奧運？」根據投票結果，該案被否決。全台灣投票權人有 44.1 % 未投票，2.6 ％ 投廢票。贊成正名的票數為 476 萬 3086 票，佔全部投票權人的 24.1 ％，否定票  577 萬 4556 票，佔 29.2 %。</p>
     <p>台灣選舉結果出爐後，中國國務院台灣事務辦公室（簡稱國台辦）發言人馬曉光在例行記者會上說：「所謂『奧運正名公投案』遭到挫敗，說明拿台灣運動員的利益作賭注不得人心，搞『台獨』註定失敗。」雖然提案領銜人紀政早在七月就表示東奧正名不等於台獨，但該案仍被部分民眾認為是對台灣認同的檢視。</p>
     <p>高雄市選情在這次縣市長選舉中備受關注，國民黨籍市長候選人韓國瑜在選舉初期不被看好，外界普遍認為民進黨籍候選人陳其邁篤定當選。但韓國瑜的聲勢「越選越高漲」，最後韓國瑜大勝陳其邁，扭轉民進黨執政高雄 20 年的局面。韓國瑜在選前就明確表態支持「九二共識」，意即「一個中國、各自表述」，這是國民黨在馬英九政府執政時期，中國大陸與台灣維持正向交流的基礎。</p>
@@ -51,7 +47,7 @@ template-color = #9e005d
   font-weight 400
   p
     margin 0
-  > h1, > h2, > h3
+  > h1, > h2
     margin 0
     font-weight 400
   > h1
@@ -67,6 +63,7 @@ template-color = #9e005d
     padding .2em
     color #fff
     font-size 1.5rem
+    font-weight 600
     background-color template-color
   > p
     font-size 1.25rem
@@ -86,25 +83,6 @@ template-color = #9e005d
       font-size 1.125rem
       text-align left
 
-  blockquote
-    display flex
-    flex-direction column
-    justify-content center
-    align-items center
-    margin 40px 0
-    > img
-      width 30px
-      align-self flex-start
-      &:nth-last-child(1)
-        transform rotate(180deg)
-        align-self flex-end
-    > p
-      font-size 20px
-      color #9e005d
-      text-align justify
-      margin 10px 0
-      br
-        display none
 .credit
   padding .5em 0
   margin-top 40px
@@ -122,6 +100,33 @@ template-color = #9e005d
     span
       & + span
         margin-left 1.5em
+
+.quotation
+  position relative
+  margin calc(50px + 2em) 0
+  color #9e005d
+  font-size 1.25rem
+  text-align justify
+  font-weight 500
+  br
+    display none
+  &::before, &::after
+    content ''
+    position absolute
+    width 30px
+    height 30px
+    background-image url(/proj-assets/vote2018-result/quotation.png)
+    background-size contain
+    background-repeat no-repeat
+  &::before
+    top -2em
+    left 0
+    background-position center bottom
+  &::after
+    bottom -2em
+    right 0
+    background-position center top
+    transform rotate(180deg)
 
 @media (min-width: 768px)
   .vote-article
@@ -153,22 +158,6 @@ template-color = #9e005d
       figcaption
         margin-top 10px
         font-size 1.25rem
-    blockquote
-      display flex
-      justify-content center
-      align-items center
-      margin 40px 0
-      > img
-        width 50px
-        &:nth-last-child(1)
-          transform rotate(180deg)
-      > p
-        font-size 30px
-        color #9e005d
-        text-align center
-        margin 0 30px
-        br
-          display initial
 
   .credit
     padding 0
@@ -178,4 +167,15 @@ template-color = #9e005d
     > img
       width 40px
       height 40px
+
+  .quotation
+    font-size 1.875rem
+    text-align center
+    br
+      display inline
+    &::before, &::after
+      top 0
+      bottom 0
+      width 50px
+      height 50px
 </style>
