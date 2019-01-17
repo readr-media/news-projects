0977d0ccc3de378ec567e2a8920261065c4e6917
Kai-Wen, Hsiung <lion15945@gmail.com>
2019-01-17 14:39:22 +0800

    add methodology

--- src/components/ElectionNews/components/Legends.vue
+++ src/components/ElectionNews/components/Legends.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="legends">
     <div class="legends__top top">
-      <p>情緒說明文字</p>
-      <p>假文假文假文假文假文假文假文假文假文假文假文假文假文</p>
+      <p>情緒計算方式說明</p>
+      <p>我們蒐集了 2018 年10月19日至 12 月 19 日的新聞，以 Google's Cloud Natural Language API 計算新聞文本的情緒分數，並將分數轉換成 0 - 10 分，再區分為三個情緒區間。</p>
+      <p class="top__more">
+        <span @click="toggleMore">（看詳細）</span>
+      </p>
+      <transition name="slide-fade">
+        <div v-show="showMore">
+          <p>新聞來源為零時政府（g0v）people-in-news <a href="https://github.com/g0v/people-in-news/blob/master/etc/news-sites.txt" target="_blank">專案</a>，共＿家來源媒體在 2018 年10月19日至 12 月 19 日的新聞。並以 2018 年縣市長候選人、公投議題等分類切成＿個關鍵字，篩選出 420180 篇新聞，以 Google's Cloud Natural Language API 計算新聞文本的情緒分數，並將分數轉換成 0 - 10 分，再區分為三個情緒區間。</p>
+          <p>對於新聞中每一段語句，Google's Cloud Natural Language API 會回傳兩個數值，分別為情緒分數 (score) ，由正面（1 分）到負面（-1 分）代表文件的整體情緒，情緒幅度 (magnitude) 則是由強（1 分）到弱（0 分），代表文件內容出現的情緒強度。為了避免非新聞內容的文字被列入統計，例如字體大小選擇、新聞作者姓名等等，我們排除了所有情緒幅度小於 0.1 的語句。新聞的情緒分數為該新聞中所有沒被排除的語句的情緒分數之平均。關鍵字的情緒分數計算方式，則為該日所有包含該關鍵字的新聞情緒分數之平均。</p>
+        </div>
+      </transition>
     </div>
     <div class="legends__bottom bottom">
       <div class="bottom__left sentiment">
@@ -23,7 +32,16 @@
 
 <script>
 export default {
-  
+  data () {
+    return {
+      showMore: false
+    }
+  },
+  methods: {
+    toggleMore () {
+      this.showMore = !this.showMore
+    }
+  }
 }
 </script>
 
@@ -34,9 +52,15 @@ export default {
 
 .top
   p
-    margin 2px 0
-    line-height 1.3
+    margin 12px 0
+    line-height 1.5
     font-size 12px
+    text-align justify
+  &__more
+    span
+      border-bottom 1px solid black
+      cursor pointer
+      user-select none
 
 .bottom
   display flex
@@ -53,4 +77,18 @@ export default {
     font-size 10px
   & + &
     margin 0 0 0 12px
+
+/* Enter and leave animations can use different */
+/* durations and timing functions.              */
+.slide-fade-enter-active {
+  transition: all .3s ease-out;
+}
+.slide-fade-leave-active {
+  transition: all .3s ease-out;
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateY(20px);
+  opacity: 0;
+}
 </style>
