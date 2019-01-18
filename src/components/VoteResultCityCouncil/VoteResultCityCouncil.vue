d2af5a19e09cd2a79833c2d06ac81062c3b416e7
Noah Tan <tempo0829@gmail.com>
2019-01-18 12:40:33 +0800

    update vote2018-result-city-council (#389)

--- src/components/VoteResultCityCouncil/VoteResultCityCouncil.vue
+++ src/components/VoteResultCityCouncil/VoteResultCityCouncil.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="city-council">
+    <Logo class="no-sprite" href="https://www.readr.tw/" top="15px" left="15px" bgImage="/proj-assets/vote2018-result-city-council/readrlogo.png" />
+    <Share :shareUrl="`${READR_SITE_URL}vote2018-result-city-council`" class="city-council__share" top="15px" right="15px" direction="down" />
     <Foreword />
     <Map />
     <Article />
+    <RelatedReports :theme="'light'"/>
   </div>
 </template>
 <script>
 
 import Article from './Article.vue'
 import Foreword from './Foreword.vue'
+import Logo from '../Logo.vue'
 import Map from './Map.vue'
+import RelatedReports from 'src/components/RelatedReports.vue'
+import Share from '../Share.vue'
 import module from 'src/store/modules/VoteResultCityCouncil'
+import { READR_SITE_URL } from '../../constants'
 
 const fetchTWJSON = (store) => {
   return store.dispatch('VoteResultCityCouncil/FETCH_TAIWAN_JSON')
@@ -21,21 +28,29 @@ export default {
   components: {
     Article,
     Foreword,
-    Map
+    Logo,
+    Map,
+    RelatedReports,
+    Share
   },
   metaInfo() {
     return {
-      title: '議會議會',
-      description: '議會議會議會議會議會議會議會議會議會議會',
+      title: '你全家都議長',
+      description: '2018 年底的大事，隨著選舉新任縣市長就職，各縣市議會也蓄勢待發。議會開議之前，最重要的是要決定議會的「老大」。議長選舉出爐，READr 帶你來看看這些議長的家庭背景，以及各地方議會有哪些家族勢力。',
       metaUrl: 'vote2018-result-city-council',
       metaImage: 'vote2018-result-city-council/og.jpg',
     };
+  },
+  data () {
+    return {
+      READR_SITE_URL,
+    }
   },
   beforeCreate () {
     this.$store.registerModule('VoteResultCityCouncil', module)
   },
   created () {
-    fetchTWJSON(this.$store)
+    // fetchTWJSON(this.$store)
   },
   mounted () {
     // ga('send', 'pageview')
@@ -45,6 +60,12 @@ export default {
   },
 }
 </script>
-<style lang="stylus" scoped>
-
+<style lang="stylus">
+  .city-council
+    padding-bottom 60px
+    &__share
+      .toggle
+        background-image url(/proj-assets/vote2018-result-city-council/share.png) !important
+        background-size 34px auto !important
+        background-position center center !important
 </style>
