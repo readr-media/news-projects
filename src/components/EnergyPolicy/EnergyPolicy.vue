39ec1c1599fab93f209b1294771c4924241a12cd
Noah Tan <tempo0829@gmail.com>
2019-03-13 19:31:55 +0800

    try to fix fullpage issue in facebook mobile browser

--- src/components/EnergyPolicy/EnergyPolicy.vue
+++ src/components/EnergyPolicy/EnergyPolicy.vue
@@ -333,7 +333,7 @@ export default {
       metaImage: 'energy-policy/og.jpg',
       customScript: `
         <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"><\/script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/vendors/scrolloverflow.min.js"><\/script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/vendors/scrolloverflow.js"><\/script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/jquery.fullpage.extensions.min.js"><\/script>
       `
     };
@@ -378,6 +378,9 @@ export default {
   mounted () {
     this.mounted = true
     this.initFullPage()
+    setTimeout(() => {
+      $.fn.fullpage.reBuild()
+    }, 500)
     ga('send', 'pageview')
   },
   methods: {
