79c4fa2a41f9ad51b04db457d7370e6cbc141342
Kai-Wen, Hsiung <lion15945@gmail.com>
2019-06-05 15:27:18 +0800

    add i18n for landing

--- src/locale/political-contribution/en.js
+++ src/locale/political-contribution/en.js
@@ -1,3 +1,16 @@
 export const POLITICAL_CONTRIBUTION = {
-
+  'LANDING': {
+    'TITLE': 'Political Donations in the Statistics',
+    'SUBTITLES': [
+      "Legislator's",
+      'eight-year',
+      'investor list'
+    ],
+    'NAVS': {
+      'CHARTS': 'Charts',
+      'EXPLORE': 'Explore',
+      'ANALYSIS': 'Analysis',
+      'DATA': 'Data'
+    }
+  }
 }
--- src/locale/political-contribution/zh-TW.js
+++ src/locale/political-contribution/zh-TW.js
@@ -1,3 +1,16 @@
 export const POLITICAL_CONTRIBUTION = {
-
+  'LANDING': {
+    'TITLE': '數讀政治獻金',
+    'SUBTITLES': [
+      '翻開立委',
+      '八年份',
+      '的金主投資名冊'
+    ],
+    'NAVS': {
+      'CHARTS': '圖表區',
+      'EXPLORE': '查找政商關係',
+      'ANALYSIS': '獻金解析文章',
+      'DATA': '原始資料'
+    }
+  }
 }
