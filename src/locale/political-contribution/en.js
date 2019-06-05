2d3fd4f8c3942dad98d89a38aeae2f4a12eccd4a
Kai-Wen, Hsiung <lion15945@gmail.com>
2019-06-05 15:58:24 +0800

    add i18n for dashboard charts

--- src/locale/political-contribution/en.js
+++ src/locale/political-contribution/en.js
@@ -19,5 +19,28 @@ export const POLITICAL_CONTRIBUTION = {
     'COMMENT': 'Comment',
     'SHARE': 'Share',
     'DONATE': 'Donate'
-  }
+  },
+  'CHARTS': [
+    [
+      'How much political contribution did the legislators receive in total?',
+      'What type of political contribution is received by each political party legislator?',
+      'Who receives the most political contributions?',
+      'Who receives the most money in the company donation?',
+    ],
+    [
+      'Who received the most corporate donations?',
+      'Who among the unselected candidates received the most political contributions?',
+      'Which industries donate the most money to the legislators?',
+      'Which industries donate most actively to the legislators?',
+    ],
+    [
+      'Which industries donate the most? To KMT or DPP?',
+      'Which industries donate continuously three-term legislator elections? Does the tendency of donating political parties change?',
+      'The current legislator is relatively easy to win the election',
+      'The election winning rate for those who run for the first time is only 9%.',
+    ],
+    [
+      'The relationship between the legislators with high political contributions and the industries',
+    ]
+  ]
 }
