48cf215e2e1fbe81106bf3711b0189f7b1dd7b24
Kai-Wen, Hsiung <lion15945@gmail.com>
2019-06-20 16:26:22 +0800

    landing: add landing desktop

---
+++ src/components/ChinaCompany/ChinaCompany.vue
@@ -0,0 +1,26 @@
+<template>
+  <section id="china-company">
+    <Landing />
+  </section>
+</template>
+
+<script>
+import Landing from './modules/landing/index.vue'
+
+export default {
+  metaInfo () {
+    return {
+      title: '中國發大財',
+      description: '',
+      metaUrl: '',
+      metaImage: ''
+    }
+  },
+  components: {
+    Landing
+  }
+}
+</script>
+
+<style lang="stylus">
+</style>
---
+++ src/components/ChinaCompany/modules/landing/components/PageStory.vue
@@ -0,0 +1,87 @@
+<template>
+  <section
+    :class="[
+      'story',
+      `story--${direction}`
+    ]"
+  >
+    <main class="story__wrapper story__img-wrapper img-wrapper">
+      <img
+        class="img-wrapper__img"
+        src=""
+        alt=""
+      >
+    </main>
+    <aside class="story__wrapper story__intro-wrapper intro-wrapper">
+      <h1
+        class="intro-wrapper__title"
+        v-text="title"
+      />
+      <p
+        class="intro-wrapper__description"
+        v-html="description"
+      />
+    </aside>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    direction: {
+      type: String,
+      default: 'normal',
+      validator (value) {
+        return ['normal', 'reverse'].includes(value)
+      }
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.story
+  height 100vh
+  background-color #000537
+  display flex
+  &--normal
+    flex-direction row
+  &--reverse
+    flex-direction row-reverse
+    & .intro-wrapper
+      align-items flex-end
+      text-align right
+  &__wrapper
+    width 50%
+
+.img-wrapper
+  &__img
+    min-width 100%
+    min-height 100%
+    object-fit cover
+    background-color black
+
+.intro-wrapper
+  display flex
+  flex-direction column
+  justify-content center
+  padding 98px
+  &__title
+    font-size 120px
+    font-weight 500
+    color #dd3c2d
+    margin 0
+  &__description
+    font-size 40px
+    color white
+    line-height 1.5
+    margin 35px 0 0 0
+</style>
