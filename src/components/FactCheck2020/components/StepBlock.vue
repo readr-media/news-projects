4c61786569fb83c8be02e3211433a2153180a21b
Noah Tan <tempo0829@gmail.com>
2019-07-18 19:29:32 +0800

    FactCheck: adjust style

--- src/components/FactCheck2020/components/StepBlock.vue
+++ src/components/FactCheck2020/components/StepBlock.vue
@@ -1,23 +1,33 @@
 <template>
   <div class="step-block">
     <p v-text="indexText"></p>
-    <div class="step-block__content">
-      <h3 v-text="contentText"></h3>
+    <div class="step-block__container">
+      <div class="step-block__text">
+        <h3 v-text="contentText"></h3>
+        <p
+          v-if="additionalText"
+          class="small"
+          v-text="additionalText"
+        />
+      </div>
       <img v-lazy="imgSrc" :alt="contentText">
     </div>
-    <p
-      v-if="additionalText"
-      class="small"
-      v-text="additionalText"
-    />
-    <a
-      v-if="link && linkText"
-      :href="link"
-      class="step-block__link"
-      target="_blank"
-      v-text="linkText"
-      @click="$emit('click')"
-    />
+    <template>
+      <a
+        v-if="link && linkText"
+        :href="link"
+        class="step-block__link"
+        target="_blank"
+        v-text="linkText"
+        @click="$emit('click')"
+      />
+      <p
+        v-else-if="!link && linkText"
+        class="step-block__link empty"
+        v-text="linkText"
+      />
+    </template>
+    
   </div>
 </template>
 <script>
@@ -56,18 +66,22 @@ export default {
       margin-top 15px
       color #9b9b9b
       line-height 1.43
-  &__content
+  &__container
     display flex
     align-items flex-start
     margin-top 20px
-    h3
-      flex 1
-      line-height 1.5
     img
       width 55px
       margin-left 25px
+  &__text
+    flex 1
+    h3
+      line-height 1.5
   &__link
     display block
+    display flex
+    justify-content center
+    align-items center
     padding 15px 0
     margin-top 15px
     color #fff
@@ -76,9 +90,21 @@ export default {
     text-decoration none
     background-color #e56300
     border-radius 6px
+    &.empty
+      color #a04500
+      background-color #f4c099
 
 @media (min-width: 768px)
   .step-block
     padding 20px
-  
+    &__text
+      max-width 320px
+
+@media (min-width: 1024px)
+  .step-block
+    &__container
+      img
+        width 85px
+        margin 0 auto
+
 </style>
