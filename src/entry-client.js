762ba362ec1e5f1b72b920aa1c4a2fed929db3af
Noah Tan <tempo0829@gmail.com>
2019-08-16 14:56:34 +0800

    update ssr data prefetching

--- src/entry-client.js
+++ src/entry-client.js
@@ -8,65 +8,19 @@ const EU = require('express-useragent')
 const userAgent = new EU.UserAgent().parse(navigator.userAgent)
 
 const debug = require('debug')('CLIENT:entry-client')
-const path = require('path')
 
 // global progress bar
 const bar = Vue.prototype.$bar = new Vue(ProgressBar).$mount()
 document.body.appendChild(bar.$el)
 
-// a global mixin that calls `asyncData` when a route component's params change
-Vue.mixin({
-  beforeRouteUpdate (to, from, next) {
-    const { asyncData } = this.$options
-    if (asyncData) {
-      asyncData({
-        store: this.$store,
-        route: to
-      }).then(next).catch(next)
-    } else {
-      next()
-    }
-  }
-})
-
-
-// prime the store with server-initialized state.
-// the state is determined during SSR and inlined in the page markup.
 if (window.__INITIAL_STATE__) {
+  // We initialize the store state with the data injected from the server
   store.replaceState(window.__INITIAL_STATE__)
 }
 
 store.state.useragent = userAgent
 
-// wait until router has resolved all async before hooks
-// and async components...
 router.onReady(() => {
-  // Add router hook for handling asyncData.
-  // Doing it after initial route is resolved so that we don't double-fetch
-  // the data that we already have. Using router.beforeResolve() so that all
-  // async components are resolved.
-  router.beforeResolve((to, from, next) => {
-    const matched = router.getMatchedComponents(to)
-    const prevMatched = router.getMatchedComponents(from)
-    let diffed = false
-    const activated = matched.filter((c, i) => {
-      return diffed || (diffed = (prevMatched[i] !== c))
-    })
-    const asyncDataHooks = activated.map(c => c.asyncData).filter(_ => _)
-    if (!asyncDataHooks.length) {
-      return next()
-    }
-
-    bar.start()
-    Promise.all(asyncDataHooks.map(hook => hook({ store, route: to })))
-      .then(() => {
-        bar.finish()
-        next()
-      })
-      .catch(next)
-  })
-
-  // actually mount to DOM
   app.$mount('#app')
 })
 
