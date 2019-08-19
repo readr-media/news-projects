e36b57fc6303da085a949ab575b83a0154a933be
Noah Tan <tempo0829@gmail.com>
2019-08-19 14:35:39 +0800

    HongKongProtests: update store module and fix message board nickname

--- src/store/modules/HongKongProtests2019/index.js
+++ src/store/modules/HongKongProtests2019/index.js
@@ -1,10 +1,15 @@
-import { appendSheet } from '../../../api'
+import Vue from 'vue'
+import { appendSheet, getSheetWithoutRedis } from '../../../api'
+import { get } from 'lodash'
 
 const SPREADSHEET_ID = '1bkpL84QQxqludEGTxSKhmJZN3eIzREZBFpEkImrRJfk'
 
 export default {
   namespaced: true,
   state: () => ({
+    spreadsheet: {
+      messages: []
+    }
   }),
   actions: {
     async ADD_MESSAGE ({}, { resource }) {
@@ -16,9 +21,23 @@ export default {
           resource: resource,
         }
       })
+    },
+    async FETCH_MESSAGES ({ commit }, { stateName, range }) {
+      return getSheetWithoutRedis({
+        params: {
+          spreadsheetId: SPREADSHEET_ID,
+          range
+        }
+      })
+        .then(res => {
+          commit('SET_MESSAGES', { stateName, data: get(res, 'body') })
+        })
+        .catch(err => err)
     }
   },
   mutations: {
-
+    SET_MESSAGES: (state, { stateName, data }) => {
+      Vue.set(state.spreadsheet, stateName, data)
+    }
   }
 }
