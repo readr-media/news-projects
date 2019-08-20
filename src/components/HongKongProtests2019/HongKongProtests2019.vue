b656e8548d0996795290a15a0b3a44e724d6b331
Kai-Wen, Hsiung <lion15945@gmail.com>
2019-08-20 18:00:06 +0800

    HongKongProtests: add hotjar script (#535)

--- src/components/HongKongProtests2019/HongKongProtests2019.vue
+++ src/components/HongKongProtests2019/HongKongProtests2019.vue
@@ -37,6 +37,18 @@ export default {
       description: '',
       metaUrl: '',
       metaImage: '',
+      customScript: `
+        <script>
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:1424298,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        <\/script>
+      `
     }
   },
   data () {
