5ea1c58c743f335a490180c23c0845d7e5d6bf22
yeefun <szyeefun@gmail.com>
2019-10-18 15:47:06 +0800

    init TableOfContents

---
+++ src/components/FoodDelivery/components/TableOfContents.vue
@@ -0,0 +1,115 @@
+<template>
+  <section class="table-of-contents">
+    <TheUser :order="contents.length" />
+    <nav>
+      <ul>
+        <li v-for="(content, idx) in contents" :key="content.id" :class="{ top: idx === 0 }">
+          <div class="table-of-contents__num">
+            <MapMarker :num="content.id" />
+          </div>
+          <div class="table-of-contents__text">
+            <p class="table-of-contents__title">{{ content.title }}</p>
+            <p class="table-of-contents__time">預估時間：{{ content.time }}</p>
+          </div>
+          <!-- <div class="table-of-contents__arrow"> -->
+          <img class="table-of-contents__arrow" src="/proj-assets/food-delivery/img/enter.svg" alt="">
+          <!-- </div> -->
+        </li>
+      </ul>
+    </nav>
+  </section>
+</template>
+
+<script>
+import TheUser from './TheUser.vue'
+import MapMarker from './MapMarker.vue'
+
+export default {
+  name: 'TableOfContents',
+  components: {
+    TheUser,
+    MapMarker
+  },
+  data () {
+    return {
+      contents: [
+        {
+          id: 1,
+          title: '外送業的難題',
+          time: '02 : 30'
+        },
+        {
+          id: 2,
+          title: '我要如何成為外送員？',
+          time: '05 : 15'
+        },
+        {
+          id: 3,
+          title: '市場龍頭大戰：開拓店家',
+          time: '07 : 56'
+        },
+        {
+          id: 4,
+          title: '消費者促銷優惠',
+          time: '06 : 38'
+        },
+        {
+          id: 5,
+          title: '為了賺更多錢剝削外送員',
+          time: '04 : 40'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+.table-of-contents
+  background-image url(/proj-assets/food-delivery/img/map.jpg)
+  background-size cover
+  background-position center
+  background-repeat no-repeat
+  min-height 100vh
+  &__num
+    width 25.42px
+    height 35.8px
+    margin-right 10px
+  &__text
+    color #4a4a4a
+    line-height normal
+  &__title
+    font-size 1.8rem
+  &__time
+    font-size 1.4rem
+  &__arrow
+    width 20px
+    margin-left auto
+  // & ul
+  //   padding-top 10px
+  //   padding-left 10px
+  //   padding-right 20px
+  //   background-color #ffdc03
+  //   border-top-left-radius 24px
+  //   border-top-right-radius 24px
+  & li
+    background-color #ffdc03
+    padding-left 10px
+    padding-right 20px
+    padding-top 10px
+    padding-bottom 10px
+    display flex
+    align-items center
+    cursor pointer
+    &.top
+      padding-top 20px
+      border-top-left-radius 24px
+      border-top-right-radius 24px
+    &:hover
+      background-color #ffec78
+      & .table-of-contents__text
+        font-weight 500
+        color #000
+      & .marker-color
+        fill #000
+</style>
---
+++ src/components/FoodDelivery/components/TheUser.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="the-user">
+    <div v-html="status" class="the-user__status"></div>
+    <img src="https://tc.sinaimg.cn/maxwidth.2048/tc.service.weibo.com/p1_pstatp_com/7f237fea1d960dd055c5fc9c43ee7e22.jpg" alt="">
+    <!-- <div v-html="status" class="op0"></div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheUser',
+  props: [ 'order' ],
+  computed: {
+    status () {
+      return `
+        <p>您有 <span>${this.order}</span> 個訂單</p>
+        <p>前往取餐</p>
+      `
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+.the-user
+  display flex
+  align-items center
+  justify-content center
+  position relative
+  padding-top 18px
+  padding-bottom 18px
+  max-width 276px
+  margin-left auto
+  margin-right auto
+  &__status
+    position absolute
+    color #fff
+    text-align right
+    font-size 1.4rem
+    line-height normal
+    left 0
+    & span
+      color #ffdc03
+  & img
+    // position absolute
+    width 50px
+    border-radius 50%
+</style>
