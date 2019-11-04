51965d5c066458beb07add239bab5bd1589e0c9e
yeefun <szyeefun@gmail.com>
2019-11-04 19:11:30 +0800

    modify description

--- src/components/ElectionBoard/ElectionBoard.vue
+++ src/components/ElectionBoard/ElectionBoard.vue
@@ -9,11 +9,10 @@
 </template>
 
 <script>
-// todo async
-import ElectionBoardData from './ElectionBoardData.vue'
-import ElectionBoardLanding from './ElectionBoardLanding.vue'
-import ElectionBoardUpload from './ElectionBoardUpload.vue'
-import ElectionBoardVerify from './ElectionBoardVerify.vue'
+// import ElectionBoardData from './ElectionBoardData.vue'
+// import ElectionBoardLanding from './ElectionBoardLanding.vue'
+// import ElectionBoardUpload from './ElectionBoardUpload.vue'
+// import ElectionBoardVerify from './ElectionBoardVerify.vue'
 
 import Logo from '../Logo.vue'
 import Share from '../Share.vue'
@@ -52,14 +51,14 @@ const fetchUserID = (store) => {
 export default {
   name: 'ElectionBoard',
   components: {
-    // ElectionBoardData: () => import('./ElectionBoardData.vue'),
-    // ElectionBoardLanding: () => import('./ElectionBoardLanding.vue'),
-    // ElectionBoardUpload: () => import('./ElectionBoardUpload.vue'),
-    // ElectionBoardVerify: () => import('./ElectionBoardVerify.vue'),
-    ElectionBoardData,
-    ElectionBoardLanding,
-    ElectionBoardUpload,
-    ElectionBoardVerify,
+    ElectionBoardData: () => import('./ElectionBoardData.vue'),
+    ElectionBoardLanding: () => import('./ElectionBoardLanding.vue'),
+    ElectionBoardUpload: () => import('./ElectionBoardUpload.vue'),
+    ElectionBoardVerify: () => import('./ElectionBoardVerify.vue'),
+    // ElectionBoardData,
+    // ElectionBoardLanding,
+    // ElectionBoardUpload,
+    // ElectionBoardVerify,
     Logo,
     Share
   },
@@ -74,7 +73,7 @@ export default {
 
     let title = '看板追追追——2020選舉看板紀錄'
     let metaImage = 'election-board/images/og-2020.jpg'
-    let description = '每到選舉季節，街上就會掛滿大大小小的候選人看板，如果候選人不申報，選舉金流的證據就會在選舉之後消失無蹤，去年我們就發現至少有 60 位候選人沒有申報自身看板的政治獻金。我們邀請你替選舉看板「打卡」，簡單三步驟：拍下照片、確認地點、標示候選人，一起為這次的選舉留下紀錄！'
+    let description = '每到選舉季節，街上就會掛滿大大小小的候選人看板，在現行制度下又不需要登記或申報，難以留下紀錄。我們邀請你替選舉看板「打卡」，簡單三步驟：拍下照片、確認地點、標示候選人，一起為這次的選舉留下紀錄！'
 
     switch (this.$route.params.params) {
       case 'upload':
@@ -88,7 +87,7 @@ export default {
       case 'data':
         // title = '看板追追追——2020選舉看板紀錄'
         metaImage = 'election-board/images/og-data-2020.jpg'
-        description = '誰掛了最多看板？每到選舉季節，街上就會掛滿大大小小的候選人看板，如果候選人不申報，選舉金流的證據就會在選舉之後消失無蹤，去年我們就發現至少有 60 位候選人沒有申報自身看板的政治獻金。一起為這次的選舉留下紀錄吧！'
+        description = '誰掛了最多看板？每到選舉季節，街上就會掛滿大大小小的候選人看板，在現行制度下又不需要登記或申報，難以留下紀錄。我們邀請你替選舉看板「打卡」，簡單三步驟：拍下照片、確認地點、標示候選人，一起為這次的選舉留下紀錄！'
         if (this.$route.query.candidate) {
           title = `看板追追追——${this.$route.query.candidate}選舉看板紀錄`
           description = `目前參選人${this.$route.query.candidate}掛了多少看板？你還有在哪裡看到${this.$route.query.candidate}的看板嗎？一起為這次的選舉留下紀錄吧！`
@@ -97,10 +96,10 @@ export default {
       case 'data-2018':
         title = '看板追追追——2018選舉看板紀錄'
         metaImage = 'election-board/images/og-data-2020.jpg'
-        description = '2018 年，READr 啟動「看板追追追」實驗，透過讀者幫街上的選舉看板「打卡」，試圖暸解實際狀況與申報資料中的黑數，我們發現，至少有 60 位候選人沒有申報自身看板的政治獻金。這代表，這些候選人如果不是用自己的財產支付這些看板的相關費用，就是違法。'
+        description = '2018 年，READr 啟動「看板追追追」實驗，透過讀者幫街上的選舉看板「打卡」，試圖暸解實際狀況與申報資料中的黑數。我們發現，2018 年至少有 60 位候選人的看板經費不是由政治獻金專戶支付，是現行制度下無法追蹤的金流。'
         if (this.$route.query.candidate) {
           title = `看板追追追——${this.$route.query.candidate}選舉看板紀錄`
-          description = `候選人${this.$route.query.candidate}掛了多少看板？${this.$route.query.candidate}在選舉政治獻金資料中有沒有申報看板的支出？一起來看看！`
+          description = `候選人${this.$route.query.candidate}掛了多少看板？${this.$route.query.candidate}的看板是由政治獻金支付的嗎？一起來看看！`
         }
         break
     }
