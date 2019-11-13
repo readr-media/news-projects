cc6465f688d26b9933fae6f08a3542b16fac6e29
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2019-11-13 18:52:53 +0800

    add article components

---
+++ src/components/UnitedFront/UnitedFront.vue
@@ -0,0 +1,26 @@
+<template>
+  <section id="unitedfront">
+    <Article />
+  </section>
+</template>
+
+<script>
+import Article from './components/Article.vue'
+
+export default {
+  metaInfo () {
+    return {
+      title: '',
+      description: '',
+      metaUrl: 'china-company',
+      metaImage: 'china-company/ogimage.png'
+    }
+  },
+  components: {
+    Article
+  }
+}
+</script>
+
+<style lang="stylus">
+</style>
---
+++ src/components/UnitedFront/components/ArticleLanding.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="article-landing"></section>
+</template>
+
+<style lang="stylus" scoped>
+.article-landing
+  height 500px
+  background-color blue
+</style>
