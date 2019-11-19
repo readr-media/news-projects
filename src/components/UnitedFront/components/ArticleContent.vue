f014d181eed5094a154b36c00c875b30c4cd002a
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2019-11-19 13:01:15 +0800

    update landing, add figure and footer components

--- src/components/UnitedFront/components/ArticleContent.vue
+++ src/components/UnitedFront/components/ArticleContent.vue
@@ -3,9 +3,20 @@
     <h1>副標要家海上童以裡定流不一臺</h1>
     <p>開孩夫合時票，多林告家、樂班春導客管在、手清以新？月腦長自，務依排面通朋好？一教。</p>
     <p>權比難展命的朋活存把方事比會廣因海行國直國不資熱布定事的，想土房相們我！全超子回包石故們標紅集，觀夜活發業東沒石和升教大是數期也自風長生。</p>
+    <ArticleContentFigure />
     <p>眼有去怎轉書紀下沒了起？業軍你車國在……議辦子中，孩代人人過生出事手部樂力把點子本、令基者老道的是長內：斯正字的馬總也地到農部？行有的地就裡期！</p>
   </article>
 </template>
+
+<script>
+import ArticleContentFigure from './ArticleContentFigure.vue'
+
+export default {
+  components: {
+    ArticleContentFigure
+  }
+}
+</script>
 
 <style lang="stylus" scoped>
 .article-content
