18cc2f870341162656888744807f75998bfdb8ba
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2019-12-09 12:22:37 +0800

    Election2020: init

---
+++ src/components/Election2020/Election2020.vue
@@ -0,0 +1,32 @@
+<template>
+  <section
+    id="election-2020"
+  >
+  </section>
+</template>
+
+<script>
+import storeModule from 'src/store/modules/Election2020'
+import { createNamespacedHelpers } from 'vuex'
+// const { mapState, mapMutations } = createNamespacedHelpers('Election2020')
+
+export default {
+  metaInfo () {
+    return {
+      title: '',
+      description: '',
+      metaUrl: 'election2020',
+      metaImage: 'election2020/ogimage.png'
+    }
+  },
+  created() {
+    this.$store.registerModule('Election2020', storeModule)
+  },
+  destroyed() {
+    this.$store.unregisterModule('Election2020')
+  }
+}
+</script>
+
+<style lang="stylus">
+</style>
---
+++ src/store/modules/Election2020/index.js
@@ -0,0 +1,8 @@
+export default {
+  namespaced: true,
+  // modules: {
+  // },
+  state: () => ({
+    test: 'test'
+  })
+}
