c6d570dfcf806e59a37548419a6e0748cae23692
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2019-12-18 16:55:35 +0800

    Election2020: add TheCredits.vue component

---
+++ src/components/Election2020/components/TheCredits.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="credits">
+    CREDIT CREDIT CREDIT CREDIT CREDIT CREDIT CREDIT CREDIT CREDIT
+  </section>
+</template>
+
+<style lang="stylus" scoped>
+.credits
+  display flex
+  justify-content center
+  align-items center
+  padding 70px 15px
+  background-color white
+
+@media (min-width 768px)
+  .credits
+    height 258px
+    padding 0
+</style>
