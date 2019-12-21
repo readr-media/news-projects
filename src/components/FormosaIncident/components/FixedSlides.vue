90711adfb5a1aaf729c7777cbef9432f255db8f5
Lin Yeefun <32699979+yeefun@users.noreply.github.com>
2019-12-21 17:31:00 +0800

    add project FormosaIncident (#699)
    * init
    
    * add FixedTimeline.vue & animation
    
    * finish opening animation
    
    * add ScrollTimeline.vue
    
    * finish FixedTimeline.vue & footer
    
    * modify picture

---
+++ src/components/FormosaIncident/components/FixedSlides.vue
@@ -0,0 +1,321 @@
+<template>
+  <article class="fixed-slides">
+    <div class="fixed-slides__picture-container" ref="pictureContainer">
+      <picture class="fixed-slides__picture first" ref="img1">
+        <source type="image/webp" media="(min-width: 720px)" srcset="/proj-assets/formosaincident/img/opening/1-desk.webp">
+        <source media="(min-width: 720px)" srcset="/proj-assets/formosaincident/img/opening/1-desk.png">
+        <source type="image/webp" srcset="/proj-assets/formosaincident/img/opening/1-mob.webp">
+        <img src="/proj-assets/formosaincident/img/opening/1-mob.png" alt="">
+      </picture>
+
+      <picture class="fixed-slides__picture" ref="img2">
+        <source type="image/webp" media="(min-width: 720px)" srcset="/proj-assets/formosaincident/img/opening/2-desk.webp">
+        <source media="(min-width: 720px)" srcset="/proj-assets/formosaincident/img/opening/2-desk.png">
+        <source type="image/webp" srcset="/proj-assets/formosaincident/img/opening/2-mob.webp">
+        <img src="/proj-assets/formosaincident/img/opening/2-mob.png" alt="">
+      </picture>
+
+      <picture class="fixed-slides__picture" ref="img3">
+        <source type="image/webp" media="(min-width: 720px)" srcset="/proj-assets/formosaincident/img/opening/3-desk.webp">
+        <source media="(min-width: 720px)" srcset="/proj-assets/formosaincident/img/opening/3-desk.png">
+        <source type="image/webp" srcset="/proj-assets/formosaincident/img/opening/3-mob.webp">
+        <img src="/proj-assets/formosaincident/img/opening/3-mob.png" alt="">
+      </picture>
+
+      <div class="fixed-slides__img-wrapper" ref="img4Wrapper">
+        <picture class="fixed-slides__picture" id="slides-img4" ref="img4">
+          <source type="image/webp" media="(min-width: 720px)" srcset="/proj-assets/formosaincident/img/opening/4-desk.webp">
+          <source media="(min-width: 720px)" srcset="/proj-assets/formosaincident/img/opening/4-desk.png">
+          <source type="image/webp" srcset="/proj-assets/formosaincident/img/opening/4-mob.webp">
+          <img class="obf-cover" src="/proj-assets/formosaincident/img/opening/4-mob.png" alt="">
+        </picture>
+        <div id="slides-img4-text" ref="img4Text">
+          <p>你這雜誌哪裡來的！<br>美麗島雜誌破壞團結，你哪裡找來的？<br>這本書我得沒收！</p>
+        </div>
+      </div>
+
+      <picture class="fixed-slides__picture" id="slides-img5" ref="img5" :class="$parent.isDeskW ? 'desk' : ''">
+        <source type="image/webp" media="(min-width: 720px)" srcset="/proj-assets/formosaincident/img/opening/5-desk.webp">
+        <source media="(min-width: 720px)" srcset="/proj-assets/formosaincident/img/opening/5-desk.png">
+        <source type="image/webp" srcset="/proj-assets/formosaincident/img/opening/5-mob.webp">
+        <img src="/proj-assets/formosaincident/img/opening/5-mob.png" alt="">
+      </picture>
+
+      <div class="fixed-slides__img-wrapper" id="slides-img6-wrapper" ref="img6Wrapper">
+        <picture class="fixed-slides__picture" id="slides-img6-1" ref="img61">
+          <source type="image/webp" media="(min-width: 720px)" srcset="/proj-assets/formosaincident/img/opening/puzzle-desk.webp">
+          <source media="(min-width: 720px)" srcset="/proj-assets/formosaincident/img/opening/puzzle-desk.png">
+          <source type="image/webp" srcset="/proj-assets/formosaincident/img/opening/puzzle-mob.webp">
+          <img class="obf-cover" src="/proj-assets/formosaincident/img/opening/puzzle-mob.png" alt="">
+        </picture>
+        <picture class="fixed-slides__title" id="slides-img6-2" ref="img62" :class="$parent.isDeskW ? 'desk' : ''">
+          <source type="image/webp" media="(min-width: 720px)" srcset="/proj-assets/formosaincident/img/opening/title-desk.webp">
+          <source media="(min-width: 720px)" srcset="/proj-assets/formosaincident/img/opening/title-desk.png">
+          <source type="image/webp" srcset="/proj-assets/formosaincident/img/opening/title-mob.webp">
+          <img class="obf-cover" src="/proj-assets/formosaincident/img/opening/title-mob.png" alt="">
+        </picture>
+      </div>
+    </div>
+
+    <div class="fixed-slides__text-container" ref="textContainer">
+      <div class="fixed-slides__text-wrapper" id="slides-text1" >
+        <p>這就是那本<br>不能讀的禁書嗎？</p>
+      </div>
+      <div class="fixed-slides__text-wrapper" ref="text2">
+        <p>「三十年來，國民黨以禁忌、神話隱蔽我們國家社會的許許多多問題⋯⋯」</p>
+        <p>許許多多問題⋯⋯是國民黨造成的？</p>
+      </div>
+      <div class="fixed-slides__text-wrapper" ref="text3">
+        <p>「民主萬歲！」民主是什麼？會影響我的生活嗎？</p>
+      </div>
+      <div class="fixed-slides__text-wrapper" id="slides-text4" ref="text4"></div>
+      <div class="fixed-slides__text-wrapper" id="slides-text5-1" ref="text51">
+        <p>40 年前，臺灣實施報禁，看禁書要把房門窗戶鎖好，看完還得燒掉</p>
+      </div>
+      <div class="fixed-slides__text-wrapper" ref="text52"></div>
+      <div class="fixed-slides__text-wrapper">
+        <p>40 年前，臺灣實施黨禁，國民黨壟斷政治權力，黨外運動卻越來越熱烈</p>
+      </div>
+      <div class="fixed-slides__text-wrapper" ref="text61"></div>
+      <div class="fixed-slides__text-wrapper">
+        <p>40 年前，臺灣的民主，因為一間雜誌社，有了截然不同的發展</p>
+      </div>
+      <div class="fixed-slides__text-wrapper" id="slides-text7" ref="text7"></div>
+      <div class="fixed-slides__text-wrapper" id="slides-text8" ref="text8"></div>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'FixedSlides',
+  mounted () {
+    this.animateOpening()
+  },
+  methods: {
+    animateOpening () {
+      const {
+        img1,
+        img2,
+        img3,
+        img4Wrapper,
+        img4,
+        img4Text,
+        img5,
+        img6Wrapper,
+        img61,
+        img62,
+        text2,
+        text3,
+        text4,
+        text51,
+        text52,
+        text61,
+        text7,
+        text8,
+        pictureContainer,
+        textContainer
+      } = this.$refs
+
+      const controller = new ScrollMagic.Controller()
+
+      const tween1 = new TimelineLite()
+      tween1.to(img1, 0.3, { opacity: 0 })
+      tween1.to(img2, 0.3, { opacity: 1, scale: 1 })
+      new ScrollMagic.Scene(this.setScrollScene(text2))
+        .setTween(tween1).addTo(controller)
+        // .addIndicators()
+
+      const tween2 = new TimelineLite()
+      tween2.to(img2, 0.3, { opacity: 0 })
+      tween2.to(img3, 0.3, { opacity: 1, scale: 1 })
+      new ScrollMagic.Scene(this.setScrollScene(text3))
+        .setTween(tween2).addTo(controller)
+        // .addIndicators()
+
+      const tween3 = new TimelineLite()
+      tween3.to(img4, 0.3, { opacity: 1, scale: 1 })
+      tween3.set(img4Text, { x: '-50%', y: '-50%' })
+      tween3.to(img4Text, 0.3, { scale: 1, opacity: 1, ease: Power4.easeOut }, 0.3)
+      new ScrollMagic.Scene(this.setScrollScene(text4, 0.8, '120%'))
+        .on('start', function (e) {
+          const isForward = e.scrollDirection === 'FORWARD'
+          TweenLite.set(img3, { opacity: isForward ? 0 : 1 })
+        })
+        .setTween(tween3).addTo(controller)
+        // .addIndicators()
+
+      const tween4 = new TimelineLite()
+      tween4.to(img4Wrapper, 0.3, { opacity: 0 })
+      tween4.to(img5, 0.3, { opacity: 1 })
+      new ScrollMagic.Scene(this.setScrollScene(text51, 0.8, '80%'))
+        .on('end', function (e) {
+          const isForward = e.scrollDirection === 'FORWARD'
+          TweenLite.set(img61, { opacity: isForward ? 1 : 0 })
+        })
+        .setTween(tween4).addTo(controller)
+        // .addIndicators()
+
+      const tween5 = TweenLite.to(img5, 0.3, { scale: 1, ease: Power2.easeOut })
+      new ScrollMagic.Scene(this.setScrollScene(text52, 0.5, '80%'))
+        .setTween(tween5).addTo(controller)
+        // .addIndicators()
+
+      const tween6 = TweenLite.to(img5, 0.3, { transformOrigin: this.$parent.ww >= 720 ? '49.88% 111%' : '50% 118%', scale: 0.8, opacity: 0, ease: Power2.easeOut })
+      new ScrollMagic.Scene(this.setScrollScene(text61, 0.5, '80%'))
+        .setTween(tween6).addTo(controller)
+        // .addIndicators()
+
+      const tween7 = new TimelineLite()
+      tween7.to(img61, 0.3, { scale: 1 })
+      tween7.to(img62, 0.45, { opacity: 1, x: 0 })
+      new ScrollMagic.Scene(this.setScrollScene(text7, 0.5, '160%'))
+        .setTween(tween7).addTo(controller)
+        // .addIndicators()
+
+      new ScrollMagic.Scene(this.setScrollScene(text8, 1, 0))
+        .on('start', (e) => {
+          const textContainerH = textContainer.getBoundingClientRect().height
+          const wh = this.$store.state.viewport[ 1 ]
+
+          if (e.scrollDirection === 'FORWARD') {
+            pictureContainer.classList.add('pos-a')
+            img6Wrapper.style.marginTop = `${textContainerH - wh}px`
+          } else {
+            pictureContainer.classList.remove('pos-a')
+            img6Wrapper.style.marginTop = 0
+          }
+        })
+        .addTo(controller)
+        // .addIndicators()
+    },
+    setScrollScene (triggerElement, triggerHook = 0.8, duration = '40%') {
+      return {
+        triggerElement,
+        triggerHook,
+        duration
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+@import '../util/global.styl'
+
+#slides
+  &-img4
+    background-image radial-gradient(circle at 50% 50%, #7a6a56, #000000 86%)
+  &-img4-text
+    position absolute
+    width 100%
+    top 50%
+    left 50%
+    transform translate(-50%, -50%) scale(0)
+    font-size 2.1rem
+    color #fff
+    line-height 2.76
+    font-weight 500
+    text-align center
+    padding-right 34px
+    padding-left 34px
+    opacity 0
+    @media (min-width $breakpoint.sm)
+      font-size 4rem
+      line-height 1.45
+  &-img5
+    // (643 / 2 + 99) / 1572
+    transform-origin 50% 26.75%
+    transform scale(4)
+    background-image none
+    &.desk
+      transform-origin 49.88% 32.95%
+  &-img6-1
+    z-index -1
+    transform scale(2)
+  &-text1
+    margin-top 20vh
+  &-text4
+    height 180vh
+  &-text5-1
+    height 140vh
+  &-text7
+    height 220vh
+  &-text8
+    height 0
+
+.fixed-slides
+  position relative
+  &__picture-container
+    position fixed
+    width 100%
+    height 100vh
+    top 0
+    left 0
+    background-color #000
+    &.pos-a
+      position absolute
+  &__picture
+    position absolute
+    top 0
+    left 0
+    width 100%
+    height 100%
+    background-image radial-gradient(circle at 50% 50%, #7a6a56, #000000 270%)
+    opacity 0
+    transform scale(1.02)
+    &.first
+      opacity 1
+      transform scale(1)
+    & img
+      width 100%
+      height 100%
+      object-fit contain
+      &.obf-cover
+        object-fit cover
+  &__title
+    position absolute
+    width 100%
+    left 0
+    bottom 31.4%
+    opacity 0
+    transform translateX(-24px)
+    &.desk
+      bottom 0
+      & img
+        max-width 640px
+    & img
+      margin-right auto
+      margin-left auto
+      max-width 320px
+      height auto
+  &__img-wrapper
+    position absolute
+    width 100%
+    height 100%
+  &__text-container
+    position relative
+    color #fff
+    font-size 1.8rem
+    line-height 2
+    @media (min-width $breakpoint.sm)
+      font-size 2.4rem
+      line-height 1.5
+  &__text-wrapper
+    text-align center
+    max-width 280px
+    margin-right auto
+    margin-left auto
+    height 100vh
+    display flex
+    flex-direction column
+    justify-content center
+    @media (min-width $breakpoint.sm)
+      max-width 700px
+    & p
+      padding 14px 22px
+      background-color rgba(0, 0, 0, 0.9)
+      box-shadow 0 2px 4px 0 rgba(0, 0, 0, 0.5)
+      @media (min-width $breakpoint.sm)
+        padding 22px 125px
+</style>
---
+++ src/components/FormosaIncident/components/FixedTimeline.vue
@@ -0,0 +1,208 @@
+<template>
+  <article class="fixed-timeline">
+    <div class="fixed-timeline__bg" ref="bg">
+      <div class="fixed-timeline__img-container">
+        <picture v-for="data in timelineData.filter((data) => data.id !== 28)" :key="`img${data.id}`" :ref="`img${data.id}`">
+          <source type="image/webp" :srcset="`/proj-assets/formosaincident/img/${data.imgSrc}-desk.webp`">
+          <img :class="{ 'obf-contain': data.imgSrc.includes('map') }" :src="`/proj-assets/formosaincident/img/${data.imgSrc}-desk.png`" alt="">
+        </picture>
+      </div>
+    </div>
+    
+    <div class="fixed-timeline__text-container" ref="textContainer">
+      <div class="fixed-timeline__text-wrapper" v-for="data in timelineData.filter((data) => data.id !== 19)" :key="`text${data.id}`" :ref="`text${data.id}`">
+        <p class="date" v-if="data.date">{{ data.date }}</p>
+        <div v-html="data.text"></div>
+        <picture class="small-map" v-if="data.smallMapName">
+          <source type="image/webp" :srcset="`/proj-assets/formosaincident/img/map/${data.smallMapName}.webp`">
+          <img :src="`/proj-assets/formosaincident/img/map/${data.smallMapName}.png`" alt="">
+        </picture>
+      </div>
+    </div>
+    <div ref="end"></div>
+  </article>
+</template>
+
+<script>
+import timelineData from '../constant/timeline.js'
+
+export default {
+  name: 'FixedTimeline',
+  data () {
+    return {
+      timelineData
+    }
+  },
+  mounted () {
+    this.fixedTimeline()
+    this.animateTimeline()
+  },
+  methods: {
+    fixedTimeline () {
+      const { textContainer, bg } = this.$refs
+
+      const controller = new ScrollMagic.Controller()
+      new ScrollMagic.Scene(this.setScrollScene(textContainer, 0, 0))
+        .on('start', (e) => {
+          if (e.scrollDirection === 'FORWARD') {
+            bg.classList.add('fixed')
+          } else {
+            bg.classList.remove('fixed')
+          }
+        })
+        .addTo(controller)
+    },
+    animateTimeline () {
+      const { end, textContainer, bg, img17, img18, img19, img27, img29, text18, text29 } = this.$refs
+      const controller = new ScrollMagic.Controller()
+      const specials = [ 18, 28 ]
+
+      for (let id = 1; id < timelineData.length; id++) {
+        if (specials.includes(id) || specials.includes(id + 1)) { continue }
+
+        const tween = new TimelineLite()
+        tween.to(this.$refs[ `img${id}` ], 0.3, { opacity: 0 })
+        tween.to(this.$refs[ `img${id + 1}` ], 0.3, { opacity: 1, scale: 1 })
+        new ScrollMagic.Scene(this.setScrollScene(this.$refs[ `text${id + 1}` ]))
+        .setTween(tween)
+        .addTo(controller)
+        // .addIndicators()
+      }
+
+      const tween1 = new TimelineLite()
+      tween1.to(img17, 0.3, { opacity: 0 })
+      tween1.to(img18, 0.3, { opacity: 1, scale: 1 })
+      tween1.to(img18, 0.3, { opacity: 0 })
+      tween1.to(img19, 0.3, { opacity: 1, scale: 1 })
+      new ScrollMagic.Scene(this.setScrollScene(text18, 0.5, '120%'))
+        .setTween(tween1)
+        .addTo(controller)
+        // .addIndicators()
+
+      const tween2 = new TimelineLite()
+      tween2.to(img27, 0.3, { opacity: 0 })
+      tween2.to(img29, 0.3, { opacity: 1, scale: 1 })
+      new ScrollMagic.Scene(this.setScrollScene(text29))
+        .setTween(tween2)
+        .addTo(controller)
+        // .addIndicators()
+      
+      new ScrollMagic.Scene(this.setScrollScene(end, 1, 0))
+        .on('start', (e) => {
+          const textContainerH = textContainer.getBoundingClientRect().height
+          const wh = this.$store.state.viewport[ 1 ]
+
+          if (e.scrollDirection === 'FORWARD') {
+            bg.classList.add('pos-a')
+            bg.style.marginTop = `${textContainerH - wh}px`
+          } else {
+            bg.classList.remove('pos-a')
+            bg.style.marginTop = 0
+          }
+        })
+        .addTo(controller)
+    },
+    setScrollScene (triggerElement, triggerHook = 0.5, duration = '40%') {
+      return {
+        triggerElement,
+        triggerHook,
+        duration
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+.fixed-timeline
+  position relative
+  &__bg
+    position absolute
+    top 0
+    left 0
+    width 100%
+    height 100vh
+    background-image linear-gradient(to bottom, #000000, #7a6a56)
+    &.fixed
+      position fixed
+    &.pos-a
+      position absolute
+    & picture
+      position absolute
+      top 0
+      right 0
+      width 100%
+      height 100%
+      opacity 0
+      transform scale(1.02)
+      &:first-child
+        opacity 1
+        transform scale(1)
+    & img
+      width 100%
+      height 100%
+      object-fit cover
+      &.obf-contain
+        object-fit contain
+  &__img-container
+    width 72%
+    height 100%
+    margin-left auto
+    position relative
+    background-color #000
+  &__text-container
+    position relative
+    width 28%
+    color #fff
+    &::before
+      content ''
+      position absolute
+      display block
+      width 2px
+      top 42vh
+      background-color #fff
+      height calc(100% - 42vh)
+      left 4.1vw
+  &__text-wrapper
+    padding-top 40vh
+    padding-bottom 40vh
+    font-size 1.6rem
+    line-height 2.19
+    margin-left 6.81vw
+    padding-right 2.71vw
+    position relative
+    &:nth-child(18)
+      padding-bottom 120vh
+    &:nth-child(27)
+      padding-bottom 0
+    & p + p
+      margin-top 35px
+    &::before, &::after
+      content ''
+      position absolute
+      display block
+      width 20px
+      height 20px
+      border-radius 50%
+      left calc(-2.71vw - 9px)
+    &::before
+      background-color #fff
+      // 35 - 20 / 2 = 7.5
+      top calc(40vh + 7.5px)
+    &::after
+      // 7.5 + 12 = 19.5
+      top calc(40vh + 19.5px)
+      border 1px solid #fff
+    & .date
+      font-size 2.1rem
+      font-weight 500
+      line-height 1.67
+      margin-bottom 20px
+    & .small-map
+      width 100%
+      margin-top 35px
+      max-width 260px
+      & img
+        width 100%
+        height auto
+</style>
