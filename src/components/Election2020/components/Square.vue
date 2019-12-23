e0d4d0e567334549265d1b91b85514fa2ecb4f5f
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2019-12-23 12:18:37 +0800

    Election2020: update Square.vue

--- src/components/Election2020/components/Square.vue
+++ src/components/Election2020/components/Square.vue
@@ -2,23 +2,35 @@
   <!-- 
     How to use this component:
     <Square
-      :showBorder="true || false"
+      :showBorder="true(this will disable animation) || false"
+      :shouldAnimate="true || false"
       :color="'black'"
     />
    -->
   <div
     :class="[
       'square',
-      { 'square--with-border': showBorder },
+      { 'square--with-number': isNumberMode },
+      { 'square--with-border': isBorderMode },
+      { 'square--animate': isAnimateMode },
       `square--${color}`
     ]"
+    v-text="numberText"
   />
 </template>
 
 <script>
 export default {
   props: {
+    number: {
+      type: Number,
+      default: -1
+    },
     showBorder: {
+      type: Boolean,
+      default: false
+    },
+    shouldAnimate: {
       type: Boolean,
       default: false
     },
@@ -37,29 +49,105 @@ export default {
         ].includes(value)
       }
     }
+  },
+  computed: {
+    numberText() {
+      return this.isNumberMode ? this.number : ''  
+    },
+    isNumberMode() {
+      return this.number !== -1
+    },
+    isBorderMode() {
+      return !this.isNumberMode && this.showBorder
+    },
+    isAnimateMode() {
+      return !this.isNumberMode && !this.isBorderMode && this.shouldAnimate
+    }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
+@keyframes animate-background {
+    0%{background-position:0% 11%}
+    50%{background-position:100% 90%}
+    100%{background-position:0% 11%}
+}
+
 .square
   d = 20px
   width d
   height d
+  animation-name animate-background
+  animation-duration 0s
+  animation-timing-function ease
+  animation-iteration-count infinite
+  &--with-number
+    display flex
+    justify-content center
+    align-items center
+    color rgba(0, 0, 0, 0.1)
+    // background-color white
+    font-size 13px
   &--with-border
     border solid 4px rgba(0, 0, 0, 0.3)
   &--black
     background-color $color-black
+    &.square--with-number
+      background none
+      background-color white
+      border solid 1px rgba(0, 0, 0, 0.1)
   &--white
     background-color #ffffff
+    &.square--with-number
+      background none
+      background-color white
+      border solid 1px rgba(0, 0, 0, 0.1)
   &--blue
-    background-color $color-blue
+    background linear-gradient(316deg, $color-blue-light, $color-blue)
+    background-size 400% 400%
+    &.square--with-border
+      background-color $color-blue
+    &.square--with-number
+      background none
+      background-color white
+      border solid 1px rgba(0, 0, 0, 0.1)
   &--green
-    background-color $color-green
+    background linear-gradient(316deg, $color-green-light, $color-green)
+    background-size 400% 400%
+    &.square--with-border
+      background-color $color-green
+    &.square--with-number
+      background none
+      background-color white
+      border solid 1px rgba(0, 0, 0, 0.1)
   &--orange
-    background-color $color-orange
+    background linear-gradient(316deg, $color-orange-light, $color-orange)
+    background-size 400% 400%
+    &.square--with-border
+      background-color $color-orange
+    &.square--with-number
+      background none
+      background-color white
+      border solid 1px rgba(0, 0, 0, 0.1)
   &--purple
-    background-color $color-purple
+    background linear-gradient(316deg, $color-purple-light, $color-purple)
+    background-size 400% 400%
+    &.square--with-border
+      background-color $color-purple
+    &.square--with-number
+      background none
+      background-color white
+      border solid 1px rgba(0, 0, 0, 0.1)
   &--gray
-    background-color $color-gray
+    background linear-gradient(316deg, $color-gray-light, $color-gray)
+    background-size 400% 400%
+    &.square--with-border
+      background-color $color-gray
+    &.square--with-number
+      background none
+      background-color white
+      border solid 1px rgba(0, 0, 0, 0.1)
+  &--animate
+    animation-duration 4s
 </style>
