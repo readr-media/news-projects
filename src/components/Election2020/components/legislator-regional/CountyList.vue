dad72fb3ec0b72145818bfa84b81a0636d89ebb4
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2019-12-24 12:38:50 +0800

    Election2020: add LegislatorRegional (#705)

---
+++ src/components/Election2020/components/legislator-regional/CountyList.vue
@@ -0,0 +1,160 @@
+<template>
+  <ul class="list">
+    <h1 class="list__title">
+      <span class="mobile">點擊選區</span><span class="desktop">移動滑鼠至選區</span>以查看詳情
+    </h1>
+    <div class="list-item-wrapper">
+      <div class="list-item-wrapper__catalog list-item-wrapper__catalog--first">
+        <CountyListItem
+          class="list__list-item"
+          :countyName="'台北市'"
+        />
+        <CountyListItem
+          class="list__list-item"
+          :countyName="'新北市'"
+        />
+        <CountyListItem
+          class="list__list-item"
+          :countyName="'桃園市'"
+        />
+        <CountyListItem
+          class="list__list-item"
+          :countyName="'台中市'"
+        />
+        <CountyListItem
+          class="list__list-item"
+          :countyName="'台南市'"
+        />
+        <CountyListItem
+          class="list__list-item"
+          :countyName="'高雄市'"
+        />
+        <CountyListItem
+          class="list__list-item"
+          :countyName="'基隆市'"
+        />
+        <CountyListItem
+          class="list__list-item"
+          :countyName="'宜蘭縣'"
+        />
+        <CountyListItem
+          class="list__list-item"
+          :countyName="'新竹縣'"
+        />
+        <CountyListItem
+          class="list__list-item"
+          :countyName="'新竹市'"
+        />
+        <CountyListItem
+          class="list__list-item"
+          :countyName="'苗栗縣'"
+        />
+        <CountyListItem
+          class="list__list-item"
+          :countyName="'彰化縣'"
+        />
+      </div>
+      <div class="list-item-wrapper__catalog list-item-wrapper__catalog--second">
+        <CountyListItem
+          class="list__list-item"
+          :countyName="'南投縣'"
+        />
+        <CountyListItem
+          class="list__list-item"
+          :countyName="'雲林縣'"
+        />
+        <CountyListItem
+          class="list__list-item"
+          :countyName="'嘉義縣'"
+        />
+        <CountyListItem
+          class="list__list-item"
+          :countyName="'嘉義市'"
+        />
+        <CountyListItem
+          class="list__list-item"
+          :countyName="'屏東縣'"
+        />
+        <CountyListItem
+          class="list__list-item"
+          :countyName="'台東縣'"
+        />
+        <CountyListItem
+          class="list__list-item"
+          :countyName="'花蓮縣'"
+        />
+        <CountyListItem
+          class="list__list-item"
+          :countyName="'澎湖縣'"
+        />
+        <CountyListItem
+          class="list__list-item"
+          :countyName="'金門縣'"
+        />
+        <CountyListItem
+          class="list__list-item"
+          :countyName="'連江縣'"
+        />
+        <CountyListItem
+          class="list__list-item"
+          :countyName="'山地原住民'"
+        />
+        <CountyListItem
+          class="list__list-item"
+          :countyName="'平地原住民'"
+        />
+      </div>
+    </div>
+  </ul>
+</template>
+
+<script>
+import CountyListItem from './CountyListItem.vue'
+
+export default {
+  components: {
+    CountyListItem
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.list
+  list-style none
+  margin 0
+  padding 0
+  &__title
+    font-size 15px
+    color $color-black-light
+    text-align left
+    margin 0 0 0 65px
+    padding 12px 10px
+    border-left solid 1px rgba(0, 0, 0, 0.4)
+    font-weight normal
+    span
+      font-weight 600
+      &.desktop
+        display none
+
+@media (min-width 768px)
+  .list
+    &__title
+      text-align right
+      border-left none
+      margin 0
+      span
+        &.mobile
+          display none
+        &.desktop
+          display inline
+
+  .list-item-wrapper
+    display flex
+    &__catalog
+      width 576px
+      &--first
+        padding 0 51px 0 0
+      &--second
+        border-left 2px solid rgba(0, 0, 0, .1)
+        padding 0 0 0 51px
+</style>
---
+++ src/components/Election2020/components/legislator-regional/LegislatorRegional.vue
@@ -0,0 +1,82 @@
+<template>
+  <section class="section">
+    <h1>2020</h1>
+    <h2>區域及原住民立委</h2>
+    <h3 v-text="h3" />
+    <CountyList
+      class="section__county-list"
+    />
+    <Legends
+      class="section__legends"
+    />
+  </section>
+</template>
+
+<script>
+import CountyList from './CountyList.vue'
+import Legends from './Legends.vue'
+
+import { createNamespacedHelpers } from 'vuex'
+const { mapGetters } = createNamespacedHelpers('Election2020')
+
+export default {
+  components: {
+    CountyList,
+    Legends
+  },
+  computed: {
+    ...mapGetters({
+      isElectionBoxOpeningStart: 'timer/isElectionBoxOpeningStart'
+    }),
+    h3() {
+      return this.isElectionBoxOpeningStart ? '各選區開票狀態' : '尚未開票'
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.section
+  h1
+    font-family $font-family-serif
+    font-size 36px
+    color $color-black
+    text-align center
+    font-weight 900
+    line-height 1
+    margin 0
+  h2
+    font-family $font-family-serif
+    font-size 21px
+    color $color-black
+    text-align center
+    font-weight bold
+    line-height 1.14
+    margin 28px 0 0 0
+  h3
+    font-size 15px
+    color $color-black
+    text-align center
+    font-weight normal
+    line-height 1.87
+    margin 11px 0 0 0
+  &__county-list
+    margin 44px 0 0 0
+  &__legends
+    margin-top 27px
+
+@media (min-width 768px)
+  .section
+    max-width 1152px
+    margin 0 auto
+    h1
+      font-size 48px
+    h2
+      font-size 30px
+      text-align left
+    h3
+      font-size 16px
+      text-align left
+    &__legends
+      margin-top 30px
+</style>
