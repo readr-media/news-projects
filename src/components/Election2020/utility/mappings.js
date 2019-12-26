1add8549a1e2aabb2fd8f00a4f4248788c0353d8
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2019-12-26 16:04:33 +0800

    Election2020: add mapPartyNameAbbr mapping (#716)

--- src/components/Election2020/utility/mappings.js
+++ src/components/Election2020/utility/mappings.js
@@ -24,3 +24,18 @@ export function mapPresidentName (store, id) {
 export function mapPartyName (store, id) {
 	return get(store, ['Election2020', 'gcs', 'data', 'party', id.toString()], '')
 }
+
+export function mapPartyNameAbbr(name) {
+  const mapping = {
+    中國國民黨: '國民黨',
+    民主進步黨: '民進黨',
+    台灣團結聯盟: '台聯黨',
+    中華統一促進黨: '統促黨',
+    天宙和平統一家庭黨: '家庭黨',
+    國會政黨聯盟: '國會黨',
+    喜樂島聯盟: '喜樂島',
+    台灣民眾黨: '民眾黨'
+  }
+
+  return mapping[name] || name
+}
