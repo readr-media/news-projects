74cf1346385f2daf445e65aa73fb6f7be9c69a4b
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2020-01-03 15:15:59 +0800

    Election2020: fix noParse issue

--- src/store/modules/firebase-modules/realtimeLegislatorsDistrictSeat.js
+++ src/store/modules/firebase-modules/realtimeLegislatorsDistrictSeat.js
@@ -1,6 +1,19 @@
 import _ from 'lodash'
 
-import { mapPartyNameAbbr } from '../../../components/Election2020/utility/mappings'
+const mapPartyNameAbbr = name => {
+  const mapping = {
+    中國國民黨: '國民黨',
+    民主進步黨: '民進黨',
+    台灣團結聯盟: '台聯黨',
+    中華統一促進黨: '統促黨',
+    天宙和平統一家庭黨: '家庭黨',
+    國會政黨聯盟: '國會黨',
+    喜樂島聯盟: '喜樂島',
+    台灣民眾黨: '民眾黨'
+  }
+
+  return mapping[name] || name
+}
 
 const module = {
   firestorePath: '/realtime-partyseat',
