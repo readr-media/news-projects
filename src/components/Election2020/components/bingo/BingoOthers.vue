57cc664429bf6d186cf5105556f2d12a13a3b740
yenyu <cyu2197@gmail.com>
2020-01-07 02:01:35 +0800

    Election2020: Add momentum rankings for legislators selected in bingo game (#763)

---
+++ src/components/Election2020/components/bingo/BingoOthers.vue
@@ -0,0 +1,13 @@
+<template>
+  <div/>
+</template>
+
+<script>
+import { mapState, mapGetters, mapMutations } from 'vuex'
+export default {
+  computed: {}
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
