f426c3d42294e4b6c967961c10ca09317ec067c1
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2020-01-10 12:47:16 +0800

    Election2020: data binding with legislator party seats

--- src/components/Election2020/components/legislator/LegislatorParty.vue
+++ src/components/Election2020/components/legislator/LegislatorParty.vue
@@ -4,17 +4,19 @@
     <h3>哪些政黨拿到了入場券</h3>
     <Chart class="section__chart" />
     <p
-      v-show="true"
+      v-if="!showResultContent"
       class="section__seat-hint"
     >
       分配席次將於中選會公布後顯示
     </p>
     <!-- Seats are shown after 1/11 -->
-    <!-- <Seats
-      v-show="true"
+    <Seats
+      v-if="showResultContent"
       class="section__seats"
       :title="'各政黨不分區席次'"
-    /> -->
+      :data="dataSeats"
+      :total="dataSeatsTotal"
+    />
   </section>
 </template>
 
@@ -22,9 +24,13 @@
 import Chart from './ChartParty.vue'
 import Seats from './Seats.vue'
 
+import _ from 'lodash'
 import { mapGetters } from 'vuex'
 import { createNamespacedHelpers } from 'vuex'
-const { mapMutations } = createNamespacedHelpers('Election2020')
+const { mapState, mapMutations } = createNamespacedHelpers('Election2020')
+const { mapState: mapStateResultToggler } = createNamespacedHelpers('electionResultToggler')
+
+import { mapPartyName, mapPartyNameAbbr } from '../../utility/mappings'
 
 export default {
   components: {
@@ -34,7 +40,30 @@ export default {
   computed: {
     ...mapGetters({
       dataChart: 'realtimeLegislatorsParty/dataChart'
-    })
+    }),
+    ...mapStateResultToggler({
+      showResultContent: state => _.get(state, [ 'data', 'showContent', 'value' ], false)
+    }),
+    ...mapState({
+      rawDataSeats: state => state.gcs.data.legislatorPartyResultSeats
+    }),
+    dataSeats() {
+      return _.map(this.rawDataSeats, (data, partyCode) => {
+        const partyCodePadded = partyCode.padStart(3, '0')
+        const partyName = mapPartyNameAbbr(mapPartyName(this.$store, partyCodePadded))
+        const seats = Math.floor(_.get(data, 'seat', 0))
+        return {
+          partyCode: partyCodePadded,
+          name: partyName,
+          seats
+        }
+      })
+      .sort((a, b) => b.seats - a.seats)
+      .filter(party => party.seats > 0)
+    },
+    dataSeatsTotal() {
+      return this.dataSeats.reduce((acc, curr) => acc + curr.seats, 0)
+    }
   },
   watch: {
     dataChart() {
