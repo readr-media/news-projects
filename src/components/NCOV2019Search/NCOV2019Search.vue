78414b99d2bb0d366847d57c004dcfda7325b74a
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2020-02-25 16:30:35 +0800

    ncov2019search: update og

--- src/components/NCOV2019Search/NCOV2019Search.vue
+++ src/components/NCOV2019Search/NCOV2019Search.vue
@@ -35,7 +35,7 @@ export default {
   metaInfo () {
     return {
       title: '武漢肺炎知識家',
-      description: '',
+      description: '搜尋或提問關於所有你想知道的嚴重特殊傳染性肺炎（俗稱武漢肺炎）最新的、最正確的資訊。武漢肺炎自 2019 年 12 月從中國湖北省武漢市開始全球延燒，READr 持續關注最新疫情，並整理各類最新資訊、驗證消息真偽，為您提供最完整的資訊。',
       metaUrl: 'ncov2019search',
       metaImage: 'ncov2019search/og.jpg'
     }
