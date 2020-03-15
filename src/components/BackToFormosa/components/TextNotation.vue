0e04095252e92c2e5aed59b59db2f629d6902708
Lin Yeefun <32699979+yeefun@users.noreply.github.com>
2020-03-15 17:49:16 +0800

    feat: 新增專題 backToFormosa (#848)
    * feat: 新增組件 SlideItem、SlideContainer
    
    * feat: 增加 slide text 動畫 & 將 bgImg 改成 picture
    
    * feat: 修改 SlideContainer、SlideItem
    
    * feat: 新增 middle content、TextNotation
    
    * feat: 新增資料 listItems
    
    * feat: 新增 ending
    
    * feat: 新增 EndingPage、after
    
    * feat: 修改 TextNotation
    
    * feat: 新增 scene 圖片
    
    * feat: 新增 ReportPageContent & SlideItem mobile layout
    
    * feat: 新增 ListPageContent & list page、EndingPage mobile layout
    
    * feat: 新增 AfterTable、TheFooter
    
    * feat: 新增 SlideItem animation
    
    * feat: 新增類別 ScrollController
    
    * feat: 新增類別 ScrollController 方法 onceScene、intervalScene
    
    * feat: 初步完成 middle 動畫
    
    * feat: 新增類別 CoveredEffect
    
    * feat: 新增 ending page 動畫
    
    * feat: 新增控制 header bar 方法

---
+++ src/components/BackToFormosa/components/TextNotation.vue
@@ -0,0 +1,70 @@
+<template>
+  <span class="text-notation">
+    <span class="text-notation__icon" @click="toggleText" :class="{ open: isText }" />
+    <span class="text-notation__text" v-if="isText">
+      <slot />
+    </span>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'TextNotation',
+  data () {
+    return {
+      isText: false
+    }
+  },
+  methods: {
+    toggleText () {
+      this.isText = !this.isText
+    } 
+  }
+}
+</script>
+
+<style lang="stylus">
+.text-notation
+  padding-right 4px
+  padding-left 4px
+  color #000
+  &__icon
+    display inline-block
+    width 18px
+    height 18px
+    box-shadow 0 2px 4px rgba(#000, 0.5)
+    background-color #fff
+    border-radius 50%
+    position relative
+    top 2px
+    cursor pointer
+    user-select none
+    &::after
+      content ''
+      display block
+      left 4px
+      top 6px
+      width 0
+      height 0
+      border-style solid
+      border-width 8px 5px 0 5px
+      border-color #a56c6a transparent transparent transparent
+      position relative
+    &.open::after
+      top 4px
+      border-width 0 5px 8px 5px
+      border-color transparent transparent #a56c6a transparent
+  &__text
+    display inline-block
+    width 100%
+    padding 10px 20px
+    box-shadow 0 2px 4px rgba(#000, 0.5)
+    background-color #fff
+    font-size 1.4rem
+    line-height 1.4
+    margin-top 12px
+    margin-bottom 12px
+    & a
+      color #a56c6a
+      font-weight 500
+</style>
---
+++ src/components/BackToFormosa/components/TheCredit.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="the-credit">
+    <p><strong>記者／</strong>陳珮瑜</p>
+    <p><strong>設計／</strong>陳怡蒨</p>
+    <p><strong>工程／</strong>林昱帆</p>
+    <p><strong>照片提供／</strong>中央社、吳三連基金會</p>
+    <p><strong>插畫／</strong>陳瑋柔</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheCredit'
+}
+</script>
+
+<style lang="stylus">
+.the-credit
+  color #333
+  line-height 1.88
+  font-size 1.6rem
+  padding-bottom 30px
+  @media (min-width 720px)
+    padding-bottom 65px
+  & strong
+    font-weight 500
+</style>
