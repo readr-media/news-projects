42a53485b46f3a00b98c39c178b175c37ac6452b
yeefun <szyeefun@gmail.com>
2020-03-23 18:56:31 +0800

    fix(BackToFormosa): fix <source> type attribute mistake

--- src/components/BackToFormosa/components/ReportPageContent.vue
+++ src/components/BackToFormosa/components/ReportPageContent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="report-page-content">
     <picture class="report-page-content__picture" ref="reportPicture">
-      <source type="image/jpg" media="(min-width: 460px)" :srcset="`/proj-assets/backtoformosa/img/${src}report-${content.name}-tl.jpg`">
+      <source type="image/jpeg" media="(min-width: 460px)" :srcset="`/proj-assets/backtoformosa/img/${src}report-${content.name}-tl.jpg`">
       <img :src="`/proj-assets/backtoformosa/img/${src}report-${content.name}-ts.jpg`" alt="">
     </picture>
     <div class="report-page-content__qa" ref="reportQa">
