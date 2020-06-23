31aed6cbd723f8c9ac8b10e5f292e6cbc24376cf
HY Tan <tempo0829@gmail.com>
2020-06-23 13:24:12 +0800

    feat: remove sw-precache-webpack-plugin(service worker) (#869)

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack')
 const merge = require('webpack-merge')
 const base = require('./webpack.base.config')
-const SWPrecachePlugin = require('sw-precache-webpack-plugin')
 const VueSSRClientPlugin = require('vue-server-renderer/client-plugin')
 
 const config = merge(base, {
@@ -45,36 +44,5 @@ const config = merge(base, {
     new VueSSRClientPlugin()
   ]
 })
-
-if (process.env.NODE_ENV === 'production') {
-  config.plugins.push(
-    // auto generate service worker
-    new SWPrecachePlugin({
-      cacheId: 'vue-hn',
-      filename: 'service-worker.js',
-      minify: true,
-      dontCacheBustUrlsMatching: /./,
-      staticFileGlobsIgnorePatterns: [/\.map$/, /\.json$/],
-      // runtimeCaching: [
-      //   {
-      //     urlPattern: '/',
-      //     handler: 'networkFirst'
-      //   },
-      //   {
-      //     urlPattern: /\/(top|new|show|ask|jobs)/,
-      //     handler: 'networkFirst'
-      //   },
-      //   {
-      //     urlPattern: '/item/:id',
-      //     handler: 'networkFirst'
-      //   },
-      //   {
-      //     urlPattern: '/user/:id',
-      //     handler: 'networkFirst'
-      //   }
-      // ]
-    })
-  )
-}
 
 module.exports = config
