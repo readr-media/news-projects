4c665dd6a340ad85922ca3229fd9cfdf1249ccf8
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2020-07-07 07:29:10 +0800

    ncov2019misinfo: fix og url

--- src/components/NCOV2019Misinfo/NCOV2019Misinfo.vue
+++ src/components/NCOV2019Misinfo/NCOV2019Misinfo.vue
@@ -12,8 +12,8 @@ export default {
     return {
       title: '五千篇事實查核報告分析：武漢肺炎假訊息戰役',
       description: 'READr 分析國際事實查核組織聯盟超過 5000 篇查核報告，探索「武漢肺炎假訊息」的危害，用視覺化的方式帶你看這 5 個月以來假訊息傳播的趨勢。',
-      metaUrl: 'ncov2019misinfo',
-      metaImage: 'ncov2019misinfo/og.png'
+      metaUrl: 'covid19-disinformation-vis',
+      metaImage: 'covid19-disinformation-vis/og.png'
     }
   },
   components: {
